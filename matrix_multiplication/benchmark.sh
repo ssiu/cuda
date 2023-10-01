3a51988c0b96e6c762b176ff72c7e1c75d68966c
Steve Siu <steve.siu@oracle.com>
2023-10-01 22:44:43 +1100

    fix

--- matrix_multiplication/benchmark.sh
+++ matrix_multiplication/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(ls | grep _mm.o)
+for file in $(ls | grep _mm.o$)
 do
     echo /usr/local/cuda-11.4/bin/ncu -f --set full -o "profile_${file}" ./${file}
     /usr/local/cuda-11.4/bin/ncu -f --set full -o "profile_${file}" ./${file}
