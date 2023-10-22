7b3b789650fa3f12526895aa58aacee29033f9a2
Steve Siu <steve.siu@oracle.com>
2023-10-22 23:17:54 +1100

    fix

--- cutlass/test.cu
+++ cutlass/test.cu
@@ -7,6 +7,15 @@ int main() {
     Layout layout = make_layout(make_shape (_2{}, _3{}),
                             make_stride(_3{}, _1{}));
     print_layout(layout);
+
+
+    Layout ThrID = Layout<Shape <_4, _2>,
+                       Stride<_1,_16>>;
+    print_layout(ThrID);
+
+    Layout CLayout = Layout<Shape <Shape <_2, _2,_2>, Shape <_2,_2, _2>>,
+                         Stride<Stride<_1,_16,_4>, Stride<_8,_2,_32>>>;
+    print(CLayout);
     return 0;
 }
 
