e15cc681dd0b2c61b7b43194515d34bc012bd187
Steve Siu <steve.siu@oracle.com>
2023-11-01 23:28:09 +1100

    Add

--- cutlass/test.cu
+++ cutlass/test.cu
@@ -4,35 +4,56 @@ using namespace cute;
 
 
 int main() {
-    Layout layout = make_layout(make_shape (_2{}, _3{}),
-                            make_stride(_3{}, _1{}));
-    print_layout(layout);
-
-
-    Layout layout_2x4 = make_layout(make_shape (2, make_shape (2,2)),
-                              make_stride(4, make_stride(2,1)));
-
-    print_layout(layout_2x4);
-
-    Layout threadID = make_layout(make_shape(4,2),
-                                make_stride(1,16));
-    print_layout(threadID);
-
-//    Layout Clayout = make_layout(make_shape(make_shape(2,2,2), make_shape(2,2,2)),
-//                                make_stride(make_stride(1,16,4), make_stride(8,2,32)));
+//    Layout layout = make_layout(make_shape (_2{}, _3{}),
+//                            make_stride(_3{}, _1{}));
+//    print_layout(layout);
+//
+//
+//    Layout layout_2x4 = make_layout(make_shape (2, make_shape (2,2)),
+//                              make_stride(4, make_stride(2,1)));
+//
+//    print_layout(layout_2x4);
+//
+//    Layout threadID = make_layout(make_shape(4,2),
+//                                make_stride(1,16));
+//    print_layout(threadID);
+//
+////    Layout Clayout = make_layout(make_shape(make_shape(2,2,2), make_shape(2,2,2)),
+////                                make_stride(make_stride(1,16,4), make_stride(8,2,32)));
+////    print_layout(CLayout);
+//    Layout layout1 = Layout<Shape <Shape <_4, _3>, _1>,
+//                         Stride<Stride<_3, _1>, _0>>{};
+//    print_layout(layout1);
+//
+//
+//    Layout CLayout = Layout<Shape <Shape <_2, _2,_2>, Shape <_2,_2, _2>>,
+//                         Stride<Stride<_1,_16,_4>, Stride<_8,_2,_32>>>{};
 //    print_layout(CLayout);
-    Layout layout1 = Layout<Shape <Shape <_4, _3>, _1>,
-                         Stride<Stride<_3, _1>, _0>>{};
-    print_layout(layout1);
+//    //Layout flat_layout = flatten(layout);
+//
+//    auto smem_atom = composition(Swizzle<2,0,3>{}, Layout<Shape<_4,_8>,Stride<_8,_1>>{});
+//    print_layout(smem_atom);
+    Layout SM70_QuadPair = Layout<Shape <_4, _2>,
+                             Stride<_1,_16>>{};
+    // (T8,V4) -> (M8,K4)
+    Layout SM70_8x4_Row  = Layout<Shape <_8,_4>,
+                                 Stride<_1,_8>>{};
+    // (T8,V4) -> (M8,K4)
+    Layout SM70_8x4_Col  = Layout<Shape <Shape <_4,_2>,_4>,
+                                 Stride<Stride<_8,_4>,_1>>{};
+    // (T8,V8) -> (M8,N8)
+    Layout SM70_8x8_16b  = Layout<Shape <_8,_8>,
+                                 Stride<_1,_8>>{};
+    // (T8,V8) -> (M8,N8)
+    Layout SM70_8x8_32b  = Layout<Shape <Shape <_2, _2,_2>,Shape <_2,_2, _2>>,
+                                 Stride<Stride<_1,_16,_4>,Stride<_8,_2,_32>>>{};
 
+    print_layout(SM70_QuadPair);
+    print_layout(SM70_8x4_Row);
+    print_layout(SM70_8x4_Col);
+    print_layout(SM70_8x8_16b);
+    print_layout(SM70_8x8_32b);
 
-    Layout CLayout = Layout<Shape <Shape <_2, _2,_2>, Shape <_2,_2, _2>>,
-                         Stride<Stride<_1,_16,_4>, Stride<_8,_2,_32>>>{};
-    print_layout(CLayout);
-    //Layout flat_layout = flatten(layout);
-
-    auto smem_atom = composition(Swizzle<2,0,3>{}, Layout<Shape<_4,_8>,Stride<_8,_1>>{});
-    print_layout(smem_atom);
 
     return 0;
 }
