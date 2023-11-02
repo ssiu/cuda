45f76714d7c5cd6e8c6e39fab64fd48f4577e88e
Steve Siu <steve.siu@oracle.com>
2023-11-02 23:19:43 +1100

    add

--- cutlass/mma_naive.cu
+++ cutlass/mma_naive.cu
@@ -8,7 +8,7 @@
 
 __global__ void matrix_multiplication(float* A, float* B, float* C) {
     int idx = threadIdx.x;
-//    printf("thread id %d\n", idx);
+    printf("thread id %d\n", idx);
     //int col = blockIdx.y * blockDim.y + threadIdx.y;
 
 
