9c350be0d0642cb7a779afaf43eb3c461ac22674
Steve Siu <steve.siu@oracle.com>
2023-11-06 23:02:47 +1100

    add

--- cutlass/flash_attn.cu
+++ cutlass/flash_attn.cu
@@ -10,19 +10,13 @@ int main() {
 
     // kBlockKSmem = 64
     // kSwizzle = 3
-    auto SmemLayoutAtomQ_1 = composition(Swizzle<3, 3, 3>{},
-                    // This has to be kBlockKSmem, using kHeadDim gives wrong results for d=128
-                    Layout<Shape<_8, _64>,
-                           Stride<_64, _1>>{});
+    auto SmemLayoutAtomQ_1 = composition(Swizzle<3, 3, 3>{}, Layout<Shape<_8, _64>, Stride<_64, _1>>{});
 
     print_layout(SmemLayoutAtomQ_1);
 
     // kBlockKSmem = 32
     // kSwizzle = 2
-    auto SmemLayoutAtomQ_2 = composition(Swizzle<2, 3, 3>{},
-                    // This has to be kBlockKSmem, using kHeadDim gives wrong results for d=128
-                    Layout<Shape<_8, _32>,
-                           Stride<_32, _1>>{});
+    auto SmemLayoutAtomQ_2 = composition(Swizzle<2, 3, 3>{},Layout<Shape<_8, _32>,Stride<_32, _1>>{});
 
     print_layout(SmemLayoutAtomQ_2);
     return 0;
