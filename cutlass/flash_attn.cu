c952b72b0365ddaab79be5b9a930f56780da602f
Steve Siu <steve.siu@oracle.com>
2023-11-06 23:07:37 +1100

    add

--- cutlass/flash_attn.cu
+++ cutlass/flash_attn.cu
@@ -10,15 +10,20 @@ int main() {
 
     // kBlockKSmem = 64
     // kSwizzle = 3
-    auto SmemLayoutAtomQ_1 = composition(Swizzle<3, 3, 3>{}, Layout<Shape<_8, _64>, Stride<_64, _1>>{});
-
-    print_layout(SmemLayoutAtomQ_1);
+//    auto SmemLayoutAtomQ_1 = composition(Swizzle<3, 3, 3>{}, Layout<Shape<_8, _64>, Stride<_64, _1>>{});
+//
+//    print_layout(SmemLayoutAtomQ_1);
 
     // kBlockKSmem = 32
     // kSwizzle = 2
-    auto SmemLayoutAtomQ_2 = composition(Swizzle<2, 3, 3>{},Layout<Shape<_8, _32>,Stride<_32, _1>>{});
+    auto SmemLayoutAtomQ = composition(Swizzle<2, 3, 3>{},Layout<Shape<_8, _32>,Stride<_32, _1>>{});
 
-    print_layout(SmemLayoutAtomQ_2);
+    auto SmemLayoutQ = decltype(tile_to_shape(
+        SmemLayoutAtomQ{},
+        Shape<4, 32>{}));
+
+    print_layout(SmemLayoutAtomQ);
+    print_layout(SmemLayoutQ);
     return 0;
 }
 
