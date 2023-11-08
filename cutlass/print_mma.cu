d48eadfafa19e6b6970ab1801eba16c5e71eed96
Steve Siu <steve.siu@oracle.com>
2023-11-08 23:11:12 +1100

    add

--- cutlass/print_mma.cu
+++ cutlass/print_mma.cu
@@ -4,13 +4,53 @@ using namespace cute;
 
 int main() {
 
-    auto tiled_mma = make_tiled_mma(SM70_8x8x4_F32F16F16F32_NT{},
+    #if 1
+    {
+        auto tiled_mma = make_tiled_mma(SM70_8x8x4_F32F16F16F32_NT{});
+
+        print_latex(tiled_mma);
+    }
+    #endif
+
+    #if 1
+    {
+        auto tiled_mma = make_tiled_mma(SM70_8x8x4_F32F16F16F32_NT{},
                                     Layout<Shape<_2,_2>>{},      // 2x2 layout of atoms (threads)
                                     Layout<Shape<_1,_1>>{},      // 1x1 layout of atoms (values)
                                     Tile<Layout<Shape <_4,_2>,   // Permutation in M
                                                 Stride<_1,_8>>>{});
 
-    print_latex(tiled_mma);
+        print_latex(tiled_mma);
+    }
+    #endif
+
+    #if 1
+    {
+        auto tiled_mma = make_tiled_mma(SM70_8x8x4_F32F16F16F32_NT{},
+                                    composition(Swizzle<1,0,-1>{},
+                                                Layout<Shape <Shape <_2,_2>,Shape <_2,_2>>,
+                                                       Stride<Stride<_1,_4>,Stride<_2,_8>>>{}),  // 4x4 layout of atoms (threads)
+                                    Layout<Shape<_2,_2>>{},                                      // 2x2 layout of atoms (values)
+                                    Tile<Layout<Shape<_4,_2,_2>,Stride<_1,_32,_8>>,              // Permutation in M
+                                         Layout<Shape<_4,_2,_2>,Stride<_1,_32,_8>>>{});          // Permutation in N
+
+        print_latex(tiled_mma);
+    }
+    #endif
+
+    #if 1
+    {
+        auto tiled_mma = make_tiled_mma(SM70_8x8x4_F32F16F16F32_NT{},
+                                    Layout<Shape<_2,_2>>{},      // 2x2 tile of atoms (threads)
+                                    Layout<Shape<_2,_2>>{},      // 2x2 tile of atoms (data)
+                                    Tile<Layout<Shape <_4,_2>,
+                                                Stride<_1,_16>>, // Permutation of M
+                                         Layout<_1,
+                                                _1>>{});         // Permutation of N
+
+        print_latex(tiled_mma);
+    }
+    #endif
     return 0;
 
 }
