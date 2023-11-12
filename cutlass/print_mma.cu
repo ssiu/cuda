075f712d93c1dbe8e13fdad27ce30a63bff5ed0e
Steve Siu <steve.siu@oracle.com>
2023-11-12 22:18:32 +1100

    add

--- cutlass/print_mma.cu
+++ cutlass/print_mma.cu
@@ -35,8 +35,8 @@ int main() {
     #if 1
     {
         auto tiled_mma = make_tiled_mma(SM70_8x8x4_F32F16F16F32_NT{},
-                                    Layout<Shape<_2, _1,_1>>{},      // 2x2 layout of atoms (threads)
-                                    Layout<Shape<_1,_1,_1>>{});
+                                    Layout<Shape<_1, _1,_1>>{},      // 2x2 layout of atoms (threads)
+                                    Layout<Shape<_2,_1,_1>>{});
 
         print_latex(tiled_mma);
     }
