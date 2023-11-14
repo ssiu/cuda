aecc03bf77da46fd3c16bfbc657aba87b6435aed
Steve Siu <steve.siu@oracle.com>
2023-11-14 21:50:08 +1100

    initial add for cute_1x1x1

---
+++ cutlass/cute_1x1x1.cu
@@ -0,0 +1,46 @@
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <cute/tensor.hpp>
+
+// using cute machinery to for 1x1x1
+
+
+// initial a 1x1x1 matrices
+
+
+// copy 1x1x1 matrix
+// copy traits
+
+__global__ void mma(float* A, float* B, float* C) {
+    printf("A = %f, B = %f", A[0], B[0]);
+}
+// do mma
+// mma traits
+
+
+int main() {
+    int size = sizeof(float);
+    // Allocate memory on the host
+    thrust::host_vector<float> h_A(1);
+    thrust::host_vector<float> h_B(1);
+    thrust::host_vector<float> h_C(1);
+
+    // Initialize matrices h_A and h_B with data
+    h_A[0] = 2;
+    h_B[0] = 3;
+
+    thrust::device_vector<float> d_A = h_A;
+    thrust::device_vector<float> d_B = h_B;
+    thrust::device_vector<float> d_C = h_C;
+
+    //call mma
+    mma<<<1,1>>>(d_A.data.get(), d_B.data.get())
+
+    cudaError_t cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        //goto Error; // Use appropriate error handling here
+    }
+
+    return 0;
+}
