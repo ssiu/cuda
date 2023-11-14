6fbe38bc27efce4d358d464662b7e8b720d0e576
Steve Siu <steve.siu@oracle.com>
2023-11-14 23:34:56 +1100

    add simple gemm cute_1x1x1

--- cutlass/cute_1x1x1.cu
+++ cutlass/cute_1x1x1.cu
@@ -12,10 +12,22 @@
 // copy 1x1x1 matrix
 // copy traits
 using namespace cute;
+using
 
-__global__ void mma(float* A, float* B, float* C) {
-    printf("A = %f, B = %f\n", A[0], B[0]);
+__global__ void mma(float* dA, float* dB, float* dC) {
+    printf("A = %f, B = %f\n", dA[0], dB[0]);
     //gemm(C[0], A[0], B[0], C[0]);
+
+    auto mA = make_tensor(make_gmem_ptr(dA), make_shape(1,1), make_stride(1, 1));      // (M,K)
+    auto mB = make_tensor(make_gmem_ptr(dB), make_shape(1,1), make_stride(1, 1));      // (N,K)
+    auto mC = make_tensor(make_gmem_ptr(dC), make_shape(1,1), make_stride(1, 1);      // (M,N)
+
+    rA = make_fragment_like(mA);
+    rB = make_fragment_like(mB);
+    rC = make_fragment_like(mC);
+
+
+    algorithm::gemm(rA, rB, rC);
 
 }
 // do mma
@@ -25,21 +37,21 @@ __global__ void mma(float* A, float* B, float* C) {
 int main() {
 
     // Allocate memory on the host
-    thrust::host_vector<float> h_A(1);
-    thrust::host_vector<float> h_B(1);
-    thrust::host_vector<float> h_C(1);
+    thrust::host_vector<float> hA(1);
+    thrust::host_vector<float> hB(1);
+    thrust::host_vector<float> hC(1);
 
     // Initialize matrices h_A and h_B with data
-    h_A[0] = 2.0f;
-    h_B[0] = 3.0f;
-    h_C[0] = 0.0f;
+    hA[0] = 2.0f;
+    hB[0] = 3.0f;
+    hC[0] = 0.0f;
 
-    thrust::device_vector<float> d_A = h_A;
-    thrust::device_vector<float> d_B = h_B;
-    thrust::device_vector<float> d_C = h_C;
+    thrust::device_vector<float> dA = hA;
+    thrust::device_vector<float> dB = hB;
+    thrust::device_vector<float> dC = hC;
 
     //call mma
-    mma<<<1,1>>>(d_A.data().get(), d_B.data().get(), d_C.data().get());
+    mma<<<1,1>>>(dA.data().get(), dB.data().get(), dC.data().get());
 
     cudaError_t cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
@@ -48,8 +60,8 @@ int main() {
     }
 
 
-    h_C = d_C;
-    printf("C = %f \n", h_C[0]);
+    hC = dC;
+    printf("C = %f \n", hC[0]);
 
 
 
