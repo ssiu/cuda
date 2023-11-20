b0d9b327f01ba52ff0b1ff112a3aa4093f8adf24
Steve Siu <steve.siu@oracle.com>
2023-11-20 23:35:16 +1100

    sm70_1x1x1

--- cutlass/sm70_1x1x1.cu
+++ cutlass/sm70_1x1x1.cu
@@ -94,7 +94,7 @@ int main() {
     hC = dC;
     for (int i=0; i< 8; i++){
         for (int j=0; j< 8; j++){
-            std::cout << hC[i*N+j] << " " ;
+            std::cout << hC[i*8+j] << " " ;
         }
         std::cout << std::endl;
     }
