0dfa46bb6ea888831f47283639830fcb76915846
Steve Siu <steve.siu@oracle.com>
2023-12-24 23:37:28 +1100

    add

--- cutlass/print_copy.cu
+++ cutlass/print_copy.cu
@@ -12,7 +12,7 @@ int main()
                                       Layout<Shape<_32,_1>>{},  // 32x1 threads
                                       Layout<Shape< _1,_4>>{}); //  1x4 values
     //const int
-    thr_copy = tiled_copy.get_thread_slice(0);
+    auto thr_copy = tiled_copy.get_thread_slice(0);
 
     print_latex(thr_copy);
   }
