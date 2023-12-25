74faef39977c3a892f47f6bd387545b812e815de
Steve Siu <steve.siu@oracle.com>
2023-12-25 17:11:58 +1100

    add

--- cutlass/sm70_32x32x32.cu
+++ cutlass/sm70_32x32x32.cu
@@ -11,52 +11,53 @@ using namespace cute;
 
 __global__ void mma_atom(half_t* dA, half_t* dB, float* dC) {
 
-    auto gA = make_tensor(make_gmem_ptr(dA), make_shape(Int<8>{}, Int<4>{}), make_stride(Int<1>{}, Int<8>{}));      // (M,K)
-    auto gB = make_tensor(make_gmem_ptr(dB), make_shape(Int<8>{}, Int<4>{}), make_stride(Int<1>{}, Int<8>{}));      // (N,K)
-    auto gC = make_tensor(make_gmem_ptr(dC), make_shape(Int<8>{}, Int<8>{}), make_stride(Int<1>{}, Int<8>{}));      // (M,N)
+    auto gA = make_tensor(make_gmem_ptr(dA), make_shape(Int<32>{}, Int<32>{}), make_stride(Int<1>{}, Int<32>{}));      // (M,K)
+    auto gB = make_tensor(make_gmem_ptr(dB), make_shape(Int<32>{}, Int<32>{}), make_stride(Int<1>{}, Int<32>{}));      // (N,K)
+    auto gC = make_tensor(make_gmem_ptr(dC), make_shape(Int<32>{}, Int<32>{}), make_stride(Int<1>{}, Int<32>{}));      // (M,N)
 
+    print_tensor(gA);
 
-    using Mma_atom = MMA_Atom<MMA_Traits<SM70_8x8x4_F16F16F16F16_NT>>;
-    using TiledMma = TiledMMA<
-        Mma_atom,
-        Layout<Shape<_1,_1,_1>>,  // 2x2x1 thread group
-        Layout<Shape<_1,_1,_1>>>; // 1x2x1 value group for 16x16x16 MMA and LDSM
-
-
-    TiledMma tiled_mma;
-
-    auto thr_mma = tiled_mma.get_thread_slice(threadIdx.x);
-
-    auto tAgA = thr_mma.partition_A(gA);
-    auto tBgB = thr_mma.partition_B(gB);
-    auto tCgC = thr_mma.partition_C(gC);
-
-    Tensor tArA  = thr_mma.partition_fragment_A(gA);
-    Tensor tBrB  = thr_mma.partition_fragment_B(gB);
-    Tensor tCrC  = thr_mma.partition_fragment_C(gC);
-
-
-    copy(tAgA, tArA);
-    copy(tBgB, tBrB);
-
-    gemm(tiled_mma, tArA, tBrB, tCrC);
-
-    if (threadIdx.x == 0) {
-//        print_tensor(gA);
-//        print_tensor(gB);
-        print_tensor(tAgA);
-        print_tensor(tBgB);
-        print_tensor(tCrC);
-//        print_tensor(tArA);
-//        print_tensor(tBrB);
-//        print_tensor(tCrC);
-    }
-
-    copy(tCrC, tCgC);
+//    using Mma_atom = MMA_Atom<MMA_Traits<SM70_8x8x4_F16F16F16F16_NT>>;
+//    using TiledMma = TiledMMA<
+//        Mma_atom,
+//        Layout<Shape<_1,_1,_1>>,  // 2x2x1 thread group
+//        Layout<Shape<_1,_1,_1>>>; // 1x2x1 value group for 16x16x16 MMA and LDSM
 //
-//    printf("thread id = %d\n", threadIdx.x);
-//    printf("tAgA = %f, tBgB = %f, tCgC = %f\n", tAgA[0], tBgB[0], tCgC[0]);
-//    printf("tArA = %f, tBrB = %f, tCrC = %f\n", tArA[0], tBrB[0], tCrC[0]);
+//
+//    TiledMma tiled_mma;
+//
+//    auto thr_mma = tiled_mma.get_thread_slice(threadIdx.x);
+//
+//    auto tAgA = thr_mma.partition_A(gA);
+//    auto tBgB = thr_mma.partition_B(gB);
+//    auto tCgC = thr_mma.partition_C(gC);
+//
+//    Tensor tArA  = thr_mma.partition_fragment_A(gA);
+//    Tensor tBrB  = thr_mma.partition_fragment_B(gB);
+//    Tensor tCrC  = thr_mma.partition_fragment_C(gC);
+//
+//
+//    copy(tAgA, tArA);
+//    copy(tBgB, tBrB);
+//
+//    gemm(tiled_mma, tArA, tBrB, tCrC);
+//
+//    if (threadIdx.x == 0) {
+////        print_tensor(gA);
+////        print_tensor(gB);
+//        print_tensor(tAgA);
+//        print_tensor(tBgB);
+//        print_tensor(tCrC);
+////        print_tensor(tArA);
+////        print_tensor(tBrB);
+////        print_tensor(tCrC);
+//    }
+//
+//    copy(tCrC, tCgC);
+////
+////    printf("thread id = %d\n", threadIdx.x);
+////    printf("tAgA = %f, tBgB = %f, tCgC = %f\n", tAgA[0], tBgB[0], tCgC[0]);
+////    printf("tArA = %f, tBrB = %f, tCrC = %f\n", tArA[0], tBrB[0], tCrC[0]);
 
 
 }
@@ -86,7 +87,7 @@ int main() {
     thrust::device_vector<float> dC = hC;
 
     //call mma
-    //mma_atom<<<1,32>>>(dA.data().get(), dB.data().get(), dC.data().get());
+    mma_atom<<<1,32>>>(dA.data().get(), dB.data().get(), dC.data().get());
 
     cudaError_t cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
