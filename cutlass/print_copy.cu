f90608f97ed2e582e1fd1cfab74141a476edfeaf
Steve Siu <steve.siu@oracle.com>
2024-01-07 23:09:52 +1100

    print_copy.cu

--- cutlass/print_copy.cu
+++ cutlass/print_copy.cu
@@ -17,7 +17,7 @@ int main()
    // print_latex(tiled_copy);
 
 
-    auto layout_2x4 = make_layout(make_shape (2, make_shape (2,2)),
+    auto layout_2x4 = make_layout(make_shape (1, make_shape (2,2)),
                               make_stride(4, make_stride(2,1)));
     auto flat_layout = flatten(layout_2x4);
     print_layout(layout_2x4);
