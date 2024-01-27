3e0efecf94bd93be630e912a330d248879cb4757
Steve Siu <steve.siu@oracle.com>
2024-01-27 22:20:12 +1100

    add

--- matrix_multiplication/mm.cu
+++ matrix_multiplication/mm.cu
@@ -67,13 +67,6 @@ __global__ void basic_mm(float* A, float* B, float* C, int N) {
         __syncthreads();
     }
 
-    if (row < N && col < N) {
-        float sum = 0.0f;
-        for (int i=0; i< N; i++){
-            sum += A[row * N + i] * B[i*N + col];
-        }
-        C[row * N + col] = sum;
-    }
 }
 
 
