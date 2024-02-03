a7b205f63a5c581b9439228b47898566317c1124
Steve Siu <steve.siu@oracle.com>
2024-02-03 16:13:17 +1100

    added vectorized_memory_access experiement

--- experiments/vectorized_memory_access.cu
+++ experiments/vectorized_memory_access.cu
@@ -1,18 +1,53 @@
 #include <iostream>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 
-int main() {
-    const int SIZE = 4;
-    float a[SIZE] = {1, 2, 3, 4};
-    float b[SIZE];
 
-    // Copying elements from array1 to array2
-    reinterpret_cast<float4*>(b)[0] = reinterpret_cast<float4*>(a)[0];
-
-    // Displaying elements of array2
-    std::cout << "Elements of b: ";
-    for (int i = 0; i < SIZE; ++i) {
-        std::cout << b[i] << " ";
+template <int N>
+__global__ void device_copy_32_kernel(int* d_in, int* d_out) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = idx; i < N; i += blockDim.x * gridDim.x) {
+        d_out[i] = d_in[i];
     }
+}
+
+
+template <int N>
+__global__ void device_copy_64_kernel(int* d_in, int* d_out) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = idx; i < N/2; i += blockDim.x * gridDim.x) {
+        reinterpret_cast<float2*>(d_out)[i] = reinterpret_cast<float2*>(d_in)[i]
+
+    }
+}
+
+template <int N>
+__global__ void device_copy_128_kernel(int* d_in, int* d_out) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = idx; i < N/4; i += blockDim.x * gridDim.x) {
+        reinterpret_cast<float4*>(d_out)[i] = reinterpret_cast<float4*>(d_in)[i]
+    }
+}
+
+
+//128 threads loading an array of size N
+int main() {
+    int N = 1024;
+    thrust::host_vector<float> h_in(N);
+    thrust::host_vector<float> h_out(N);
+
+    for (int i=0; i< N; i++){
+        h_in[i] = 1.0f;
+    }
+
+    thrust::device_vector<float> d_in = h_in;
+    thrust::device_vector<float> d_out = h_out;
+
+
+    device_copy_32_kernel<N><<<128,4>>>(d_in.data().get(), d_out.data().get());
+    device_copy_64_kernel<N><<<128,4>>>(d_in.data().get(), d_out.data().get());
+    device_copy_128_kernel<N><<<128,4>>>(d_in.data().get(), d_out.data().get());
+
 
     return 0;
 }
