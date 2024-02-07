2c26883f176865da6edb940d7d7022195de0c056
Steve Siu <steve.siu@oracle.com>
2024-02-07 23:32:12 +1100

    added bash script

---
+++ experiments/launch_vectorized_memory_access.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start with 1024 (2^10)
+number=$((2**10))
+
+# Set a maximum limit if necessary (e.g., less than 2^20)
+max_limit=$((2**12))
+
+while [ $number -le $max_limit ]; do
+  echo Currently profiling array of size $number
+  nvprof --csv --log-file "profile_$number.csv" ./vectorized_memory_access.cu
+  number=$((number * 2))
+done
+
+
