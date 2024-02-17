85188b400c0de87c21fec04e3b477ad7b45683ae
Steve Siu <steve.siu@oracle.com>
2024-02-17 21:53:48 +1100

    fi

--- experiments/ilp.cu
+++ experiments/ilp.cu
@@ -4,8 +4,9 @@
 //            number of ILP
 
 __global__ void arithmetic_kernel(int num_instructions, int N) {
-    #pragma unroll 1
     int a = 1;
+
+    #pragma unroll 1
     for (int i = 0; i < N; i ++) {
         a = a * 1 + 1;
     }
