2655571563a26590a9c2eead677d438d0be4fcdf
Steve Siu <steve.siu@oracle.com>
2024-02-19 21:34:23 +1100

    fix ilp

--- experiments/ilp.cu
+++ experiments/ilp.cu
@@ -10,165 +10,55 @@
 
 // #define N 1000000000
 
-__global__ void naive_transpose(float* d_in, float* d_out, int N) {
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ void test_1(float a, float c) {
+        float a1 = a;
+        float c1 = c;
 
-    d_out[col * N + row] = d_in[row * N + col];
-    int a = 0;
-
-    for (int i = 0; i<10; i++){
-        a = a * i + i;
-    }
-
-    d_out[col * N + row] = a;
-
-}
-
-
-__global__ void naive_transpose_1(float* d_in, float* d_out, int N) {
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-
-    d_out[col * N + row] = d_in[row * N + col];
-    int array[10];
-
-    #pragma unroll
-    for (int i = 0; i<10; i++){
-        d_out[i] = i;
-    }
-
-
-
-}
-
-
-//__global__ void arithmetic_kernel_1(float x) {
-//    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-//
-//    int a = idx + x;
-//
-//    //#pragma unroll 1
-////    for (int i = 0; i < N; i ++) {
-////        a = a * 1 + 1;
-////    }
-//}
-
-
-//__global__ void arithmetic_kernel_2() {
-//    int a = 1;
-//    int b = 1;
-//
-//    //#pragma unroll 1
-//    for (int i = 0; i < N; i ++) {
-//        a = a * 1 + 1;
-//        b = b * 1 + 1;
-//    }
-//}
-//
-//__global__ void arithmetic_kernel_3() {
-//    int a = 1;
-//    int b = 1;
-//    int c = 1;
-//
-//    //#pragma unroll 1
-//    for (int i = 0; i < N; i ++) {
-//        a = a * 1 + 1;
-//        b = b * 1 + 1;
-//        c = c * 1 + 1;
-//    }
-//}
-//
-//__global__ void arithmetic_kernel_4() {
-//    int a = 1;
-//    int b = 1;
-//    int c = 1;
-//    int d = 1;
-//
-//    //#pragma unroll 1
-//    for (int i = 0; i < N; i ++) {
-//        a = a * 1 + 1;
-//        b = b * 1 + 1;
-//        c = c * 1 + 1;
-//        d = d * 1 + 1;
-//    }
-//}
-//
-//__global__ void arithmetic_kernel_5() {
-//    int a = 1;
-//    int b = 1;
-//    int c = 1;
-//    int d = 1;
-//    int e = 1;
-//
-//    #pragma unroll
-//    for (int i = 0; i < N; i ++) {
-//        a = a * 1 + 1;
-//        b = b * 2 + 1;
-//        c = c * 3 + 1;
-//        d = d * 4 + 1;
-//        e = e * 5 + 1;
-//    }
-//}
-
-thrust::host_vector<float> generateMatrix(int N) {
-    thrust::host_vector<float> A(N * N);
-
-    // Create random engine
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    // Define distribution range
-    std::uniform_real_distribution<float> dis(0.0, 1.0);
-
-    // Generate random matrix
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<N; j++) {//
-            A[i * N + j] = static_cast<float>(i*N+j);
+        #pragma unroll 1
+        for (int i = 0; i < 1024; i++)
+        {
+                c1 += a1;
         }
-    }
+        if (c1 == 0) printf("!");
+}
 
-    // Return both matrices
-    return A;
+
+
+__global__ void test_5(float a, float c) {
+        float a1 = a;
+        float c1 = c;
+        float a2 = a+1;
+        float c2 = c+1;
+        float a3 = a+2;
+        float c3 = c+2;
+        float a4 = a+3;
+        float c4 = c+3;
+        float a5 = a+4;
+        float c5 = c+4;
+
+        #pragma unroll 1
+        for (int i = 0; i < 1024; i++)
+        {
+                c1 += a1;
+                c2 += a2;
+                c3 += a3;
+                c4 += a4;
+                c5 += a5;
+        }
+        if (c1 == 0) printf("?");
+        if (c2 == 0) printf("!");
+        if (c3 == 0) printf(".");
+        if (c4 == 0) printf("*");
+        if (c5 == 0) printf("/");
 }
 
 
 
 int main(){
-    int N = 1024;
 
-//    arithmetic_kernel_1<<<numBlocks, numThreads>>>();
-//    arithmetic_kernel_2<<<numBlocks, numThreads>>>();
-//    arithmetic_kernel_3<<<numBlocks, numThreads>>>();
-//    arithmetic_kernel_4<<<numBlocks, numThreads>>>();
-//    arithmetic_kernel_5<<<numBlocks, numThreads>>>();
+    test1<<<256, 256>>>(0.1, 0.2);
+    test5<<<256, 256>>>(0.1, 0.2);
 
-
-    auto h_in = generateMatrix(N);
-    //thrust::host_vector<float> h_in(N * N);
-    thrust::host_vector<float> h_out(N * N);
-    //thrust::host_vector<float> h_tranpose(N * N);
-
-
-    thrust::device_vector<float> d_in = h_in;
-    thrust::device_vector<float> d_out = h_out;
-
-    //call mma
-    //mma_atom<<<1,1>>>(dA.data().get(), dB.data().get(), dC.data().get());
-
-    dim3 dimGrid(32, 32); // You can adjust this based on your GPU's capability
-    dim3 dimBlock(32, 32);
-
-    naive_transpose<<<dimGrid, dimBlock>>>(d_in.data().get(), d_out.data().get(), N);
-    naive_transpose_1<<<dimGrid, dimBlock>>>(d_in.data().get(), d_out.data().get(), N);
-
-    cudaError_t cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        //goto Error; // Use appropriate error handling here
-    }
-
-    h_out = d_out;
 
     return 0;
 
