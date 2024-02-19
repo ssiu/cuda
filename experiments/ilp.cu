be806a9389ae8a6bff995d940dc14b303d06df76
Steve Siu <steve.siu@oracle.com>
2024-02-19 22:09:56 +1100

    fix ilp

--- experiments/ilp.cu
+++ experiments/ilp.cu
@@ -14,7 +14,6 @@ __global__ void test_1(float a, float c) {
         float a1 = a;
         float c1 = c;
 
-        #pragma unroll 1
         for (int i = 0; i < N; i++)
         {
                 c1 += a1;
