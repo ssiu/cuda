e966b8fc058868fa377ee42742882b16600f24c8
Steve Siu <steve.siu@oracle.com>
2024-02-23 22:51:24 +1100

    fix

--- matrix_multiplication/mm_0.cu
+++ matrix_multiplication/mm_0.cu
@@ -6,7 +6,7 @@ __global__ void mm_0(float* A, float* B, float* C, int N){
     int row = threadIdx.x + blockDim.x * blockIdx.x;
     int col = threadIdx.y + blockDim.y * blockIdx.y;
 
-    for (int i = 0; i< N; i++){
+    for (int i = 0; i < N; i++){
         C[row*N + col] = A[row*N + i] * B[i*N + col];
     }
 }
