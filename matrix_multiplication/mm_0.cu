0807fd8bf8d17b142b6b446fae1b760d15d8969f
Steve Siu <steve.siu@oracle.com>
2024-02-23 22:36:17 +1100

    fix

--- matrix_multiplication/mm_0.cu
+++ matrix_multiplication/mm_0.cu
@@ -1,11 +1,14 @@
 // naive matrix multiplication
+#include <iostream>
 
 __global__ void mm_0(float* A, float* B, float*C, int N){
     //int threadId = threadIdx.x + blockDim.x * threadIdx.y
     int row = threadIdx.x + blockDim.x * blockIdx.x;
     int col = threadIdx.y + blockDim.y * blockIdx.y;
 
-    for (int i = 0; i< N; i++){
-        C[row*N + col] = A[row*N + i] * B[i*N + col];
-    }
+    printf("row: %d, col: %d\n", row, col);
+
+//    for (int i = 0; i< N; i++){
+//        C[row*N + col] = A[row*N + i] * B[i*N + col];
+//    }
 }
