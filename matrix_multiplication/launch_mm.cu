15150f753a91fff050d827c40457e15a8770ef89
Steve Siu <steve.siu@oracle.com>
2024-02-24 22:29:12 +1100

    fix

--- matrix_multiplication/launch_mm.cu
+++ matrix_multiplication/launch_mm.cu
@@ -10,53 +10,53 @@
 
 
 int main(){
-    int N = 1024;
-
-    thrust::host_vector<float> hA = generateMatrices(N);
-    thrust::host_vector<float> hB = generateMatrices(N);
-    thrust::host_vector<float> hC(N*N);
-    thrust::host_vector<float> hC_cublas(N*N);
-
-    thrust::device_vector<float> dA = hA;
-    thrust::device_vector<float> dB = hB;
-    thrust::device_vector<float> dC = hC;
-    thrust::device_vector<float> dC_cublas(N*N);
-
-
-    dim3 dimGrid(32, 32);
-    dim3 dimBlock(32, 32);
-    mm_0<<<dimGrid, dimBlock>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
-                                    thrust::raw_pointer_cast(dC.data()), N);
-    hC = dC;
-
-    //
-    // cublas
-    //
-    float alpha = 1.0f;
-    float beta = 1.0f;
-
-    cudaError_t cudaStat;  // cudaMalloc status
-    cublasStatus_t stat;   // cuBLAS functions status
-    cublasHandle_t handle; // cuBLAS context
-    cublasCreate(&handle);
-    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, N, N, N, &alpha, thrust::raw_pointer_cast(dB.data()), N,
-                     thrust::raw_pointer_cast(dA.data()), N, &beta, thrust::raw_pointer_cast(dC_cublas.data()), N);
-
-    hC_cublas = dC_cublas;
-
-    cublasDestroy(handle);
-
-    for (int i=0;i<100;i++){
-        std::cout << "check A and B matrices: " << hA[i] << " " << hB[i] << std::endl;
-    }
-
-    for (int i=0;i<100;i++){
-        std::cout << "compare results against cublas: " << hC[i] << " " << hC_cublas[i] << std::endl;
-    }
-
-    if (isSameMatrices(hC.data(), hC_cublas.data(), N)==0){
-        std::cout << "Wrong answer" << std::endl;
-    }
+//    int N = 1024;
+//
+//    thrust::host_vector<float> hA = generateMatrices(N);
+//    thrust::host_vector<float> hB = generateMatrices(N);
+//    thrust::host_vector<float> hC(N*N);
+//    thrust::host_vector<float> hC_cublas(N*N);
+//
+//    thrust::device_vector<float> dA = hA;
+//    thrust::device_vector<float> dB = hB;
+//    thrust::device_vector<float> dC = hC;
+//    thrust::device_vector<float> dC_cublas(N*N);
+//
+//
+//    dim3 dimGrid(32, 32);
+//    dim3 dimBlock(32, 32);
+//    mm_0<<<dimGrid, dimBlock>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
+//                                    thrust::raw_pointer_cast(dC.data()), N);
+//    hC = dC;
+//
+//    //
+//    // cublas
+//    //
+//    float alpha = 1.0f;
+//    float beta = 1.0f;
+//
+//    cudaError_t cudaStat;  // cudaMalloc status
+//    cublasStatus_t stat;   // cuBLAS functions status
+//    cublasHandle_t handle; // cuBLAS context
+//    cublasCreate(&handle);
+//    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, N, N, N, &alpha, thrust::raw_pointer_cast(dB.data()), N,
+//                     thrust::raw_pointer_cast(dA.data()), N, &beta, thrust::raw_pointer_cast(dC_cublas.data()), N);
+//
+//    hC_cublas = dC_cublas;
+//
+//    cublasDestroy(handle);
+//
+//    for (int i=0;i<100;i++){
+//        std::cout << "check A and B matrices: " << hA[i] << " " << hB[i] << std::endl;
+//    }
+//
+//    for (int i=0;i<100;i++){
+//        std::cout << "compare results against cublas: " << hC[i] << " " << hC_cublas[i] << std::endl;
+//    }
+//
+//    if (isSameMatrices(hC.data(), hC_cublas.data(), N)==0){
+//        std::cout << "Wrong answer" << std::endl;
+//    }
 
 // cublas experiment
     thrust::host_vector<float> hA = {1.0, 2.0, 3.0, 4.0};
