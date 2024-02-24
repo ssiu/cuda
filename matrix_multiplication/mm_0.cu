a015d7fd9662badf39b3907f8e962eb8b49391fe
Steve Siu <steve.siu@oracle.com>
2024-02-24 22:01:33 +1100

    fix

--- matrix_multiplication/mm_0.cu
+++ matrix_multiplication/mm_0.cu
@@ -7,6 +7,9 @@ __global__ void mm_0(float* A, float* B, float* C, int N){
     int col = threadIdx.y + blockDim.y * blockIdx.y;
     int sum = 0;
     for (int i = 0; i < N; i++){
+        if (blockIdx.x == 0 and blockIdx.x==0 and threadIdx.x==0 and threadIdx.y==0){
+            printf("A IS %f, B is %f\n", A[row*N + i], B[i*N + col])
+        }
         sum += A[row*N + i] * B[i*N + col];
     }
     C[row*N + col] = sum;
