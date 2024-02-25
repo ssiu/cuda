b751a42a006f6c672e402560b415edfd0c1cc4f2
Steve Siu <steve.siu@oracle.com>
2024-02-25 15:51:34 +1100

    add m1

--- matrix_multiplication/mm.cuh
+++ matrix_multiplication/mm.cuh
@@ -2,6 +2,6 @@
 #define MATRIX_MULTIPLICATION_KERNELS
 
 __global__ void mm_0(float* A, float* B, float*C, int N);
-//__global__ void mm_1(float* A, float* B, float*C, int N);
+__global__ void mm_1(float* A, float* B, float*C, int N);
 
 #endif
