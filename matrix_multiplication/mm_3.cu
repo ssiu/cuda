c9f00acdd5bde252b06f28f5ae4c1f7eb234d66c
Steve Siu <steve.siu@oracle.com>
2024-02-28 22:28:23 +1100

    fix

--- matrix_multiplication/mm_3.cu
+++ matrix_multiplication/mm_3.cu
@@ -37,8 +37,8 @@ __global__ void mm_3(float* A, float* B, float* C, int N){
         gCol_A = kTile*TILE_WIDTH / 4 + threadIdx.x;
         gRow_B = kTile*TILE_WIDTH / 4 + threadIdx.y;
         // bank conflict free G->S
-//        reinterpret_cast<float4*>(sA)[sRow * TILE_WIDTH / 4 + sCol] = reinterpret_cast<float4*>(A)[gRow_A * N / 4 + gCol_A];
-//        reinterpret_cast<float4*>(sB)[sRow * TILE_WIDTH / 4 + sCol] = reinterpret_cast<float4*>(B)[gRow_B * N / 4 + gCol_B];
+        reinterpret_cast<float4*>(sA)[sRow * TILE_WIDTH / 4 + sCol] = reinterpret_cast<float4*>(A)[gRow_A * N / 4 + gCol_A];
+        reinterpret_cast<float4*>(sB)[sRow * TILE_WIDTH / 4 + sCol] = reinterpret_cast<float4*>(B)[gRow_B * N / 4 + gCol_B];
 //
 //        __syncthreads();
 //
