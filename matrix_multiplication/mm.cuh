b29af8df9ef9e7fd7991e12ec90e5aada63d0d2b
Steve Siu <steve.siu@oracle.com>
2024-03-12 21:56:34 +1100

    fix

--- matrix_multiplication/mm.cuh
+++ matrix_multiplication/mm.cuh
@@ -7,5 +7,6 @@ __global__ void mm_2(float* A, float* B, float*C, int N);
 __global__ void mm_3(float* A, float* B, float*C, int N);
 __global__ void mm_4(float* A, float* B, float*C, int N);
 __global__ void mm_5(float* A, float* B, float*C, int N);
+__global__ void mm_6(float* A, float* B, float*C, int N);
 
 #endif
