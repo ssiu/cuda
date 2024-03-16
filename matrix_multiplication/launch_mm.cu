69b6bf7ef8eb0ada8cddfc18f901ac1530c0a9df
Steve Siu <steve.siu@oracle.com>
2024-03-16 15:44:06 +1100

    fix

--- matrix_multiplication/launch_mm.cu
+++ matrix_multiplication/launch_mm.cu
@@ -96,7 +96,7 @@ int main(){
 //        }
 
         for (int i=0;i<100;i++){
-            std::cout << hC[i] << " " << hC_cublas[i] << std::endl;
+            std::cout << i << hC[i] << " " << hC_cublas[i] << std::endl;
         }
 //        int num = countZeros(hC.data(), N);
 //        std::cout << "number of zeros in hC is " << num << std::endl;
