7a975e659c0920a6ba6d383d1e632294d59b1517
Steve Siu <steve.siu@oracle.com>
2024-03-22 15:51:05 +1100

    fix

--- matrix_multiplication/mm_9.cu
+++ matrix_multiplication/mm_9.cu
@@ -29,13 +29,13 @@
 #define TILE_WIDTH 128
 #define thread_id threadIdx.x
 #define warp_id (threadIdx.x >> 5)
-#define lane_id (threadIdx.x % 32)
+#define lane_id (threadIdx.x & 31)
 
 // warp tiling
 #define warp_row (warp_id >> 1) * 32
-#define warp_col (warp_id % 2) * 64
+#define warp_col (warp_id & 1) * 64
 #define thread_row (lane_id >> 3)
-#define thread_col (lane_id % 8) * 4
+#define thread_col (lane_id & 7) * 4
 
 
 #define gC_row TILE_WIDTH * blockIdx.y
@@ -43,9 +43,9 @@
 
 // shared memory offsets
 #define sA_row (thread_id >> 1)
-#define sA_col (thread_id % 2) * 4
+#define sA_col (thread_id & 1) * 4
 #define sB_row (threadIdx.x >> 5)
-#define sB_col (threadIdx.x % 32) * 4
+#define sB_col (threadIdx.x & 31) * 4
 //
 #define gA_row (gC_row + sA_row)
 #define gA_col ((kBlock + 1) * BLOCK_WIDTH + sA_col)
