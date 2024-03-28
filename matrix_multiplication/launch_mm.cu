6e71d6db728c3daa7334d3585074cee9ca3401c7
Steve Siu <steve.siu@oracle.com>
2024-03-28 21:31:53 +1100

    fix

--- matrix_multiplication/launch_mm.cu
+++ matrix_multiplication/launch_mm.cu
@@ -66,8 +66,8 @@ int main(){
                                thrust::raw_pointer_cast(dC.data()), N);
 
 
-//    mm_8<<<dimGrid4, dimBlock4>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
-//                               thrust::raw_pointer_cast(dC.data()), N);
+    mm_8<<<dimGrid4, dimBlock4>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
+                               thrust::raw_pointer_cast(dC.data()), N);
 //    mm_9<<<dimGrid4, dimBlock4>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
 //                               thrust::raw_pointer_cast(dC.data()), N);
     hC = dC;
