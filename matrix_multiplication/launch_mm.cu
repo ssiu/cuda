e8bb86442c274caae700e1b7e7f27329b4601ce1
Steve Siu <steve.siu@oracle.com>
2024-04-11 22:05:49 +1000

    fix

--- matrix_multiplication/launch_mm.cu
+++ matrix_multiplication/launch_mm.cu
@@ -59,11 +59,11 @@ int main(){
 ////// Host code
 //    int maxbytes = 98304; // 96 KB
 //    cudaFuncSetAttribute(mm_6, cudaFuncAttributeMaxDynamicSharedMemorySize, maxbytes);
-//    mm_6<<<dimGrid4, dimBlock4, maxbytes>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
-//                                   thrust::raw_pointer_cast(dC.data()), N);
+    mm_6<<<dimGrid4, dimBlock4, maxbytes>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
+                                   thrust::raw_pointer_cast(dC.data()), N);
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-    mm_7<<<dimGrid4, dimBlock4>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
-                               thrust::raw_pointer_cast(dC.data()), N);
+//    mm_7<<<dimGrid4, dimBlock4>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
+//                               thrust::raw_pointer_cast(dC.data()), N);
 
 //    mm_8<<<dimGrid4, dimBlock4>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
 //                               thrust::raw_pointer_cast(dC.data()), N);
