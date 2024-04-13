01ea3cd1321feef4d951d9c4a7e1cbae17e770b8
Steve Siu <steve.siu@oracle.com>
2024-04-13 22:30:45 +1000

    fix

--- matrix_multiplication/launch_mm.cu
+++ matrix_multiplication/launch_mm.cu
@@ -85,7 +85,7 @@ int main(){
         dim3 gridDim_mm_new_1(N / TILE_WIDTH,N / TILE_WIDTH);
         dim3 blockDim_mm_new_1(TILE_WIDTH,TILE_WIDTH);
         mm_new_1<<<gridDim_mm_new_1, blockDim_mm_new_1>>>(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()),
-                               thrust::raw_pointer_cast(dC.data()), N)
+                               thrust::raw_pointer_cast(dC.data()), N);
         hC = dC;
     }
     #endif
