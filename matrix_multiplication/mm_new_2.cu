f189ba440dfebb61171a830a6a2f6d22b1cebaa4
Steve Siu <steve.siu@oracle.com>
2024-04-16 15:58:27 +1000

    fix

--- matrix_multiplication/mm_new_2.cu
+++ matrix_multiplication/mm_new_2.cu
@@ -3,14 +3,14 @@
 #define TILE_WIDTH 32
 
 
-//__device__ void loadFromGmem(){
-//
-//}
-//
-//__device__ void storeToSmem(){
-//
-//}
-//
+__device__ void loadFromGmem(float* M, float* r, int addr){
+    *r = M[addr];
+}
+
+__device__ void storeToSmem(float* r, float* sM, int addr){
+    sM[addr] = *r;
+}
+
 //__device__ void loadFromSmem(){
 //
 //
@@ -35,21 +35,27 @@ __global__ void mm_new_2(float* A, float* B, float* C, int N){
     C = &C[gRow*N + gCol];
     __shared__ float sA[TILE_WIDTH*TILE_WIDTH];
     __shared__ float sB[TILE_WIDTH*TILE_WIDTH];
+    float rA;
+    float rB;
 
     float sum = 0;
     for (int kBlock=0; kBlock<N/TILE_WIDTH; kBlock++){
+//        sA[sPos] = A[gPos];
+//        sB[sPos] = B[gPos];
+
         //load from gmem
-        sA[sPos] = A[gPos];
-        sB[sPos] = B[gPos];
+        loadFromGmem(A, &rA, gPos);
+        loadFromGmem(B, &rB, gPos);
 
         // store to sram
+        storeToSmem(&rA, sA, sPos);
+        storeToSmem(&rB, sB, sPos);
 
         //shift A,B pointers
         __syncthreads();
         A += TILE_WIDTH;
         B += TILE_WIDTH * N;
         // sync thread
-
 
         for (int k=0; k<TILE_WIDTH; k++) {
             sum += sA[ty*TILE_WIDTH + k] * sB[k*TILE_WIDTH + tx];
