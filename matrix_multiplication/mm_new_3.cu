b3584dd7d94a30a42bd8c12b45e212778fc904f5
Steve Siu <steve.siu@oracle.com>
2024-04-19 23:59:46 +1000

    fix

--- matrix_multiplication/mm_new_3.cu
+++ matrix_multiplication/mm_new_3.cu
@@ -102,18 +102,21 @@ __global__ void mm_new_3(float* A, float* B, float* C, int N){
         A += BLOCK_WIDTH;
         B += BLOCK_WIDTH * N;
         // sync thread
+        if (thread_id==2) {
+            print(sA_gOffset);
+        }
 
-        if (thread_id==0) {
-            //printf("thread is %d, kBlock is %d, kFragment is %d, frag_A is %f\n", 1, kBlock, kFragment, fragment_A[i]);
-//            for (int i=8;i<16; i++){
+//        if (thread_id==0) {
+//            //printf("thread is %d, kBlock is %d, kFragment is %d, frag_A is %f\n", 1, kBlock, kFragment, fragment_A[i]);
+////            for (int i=8;i<16; i++){
+////                printf("%d %f\n", j, sA[i]);
+////                j++;
+////            }
+//            for (int i=0;i<8; i++){
 //                printf("%d %f\n", j, sA[i]);
 //                j++;
 //            }
-            for (int i=0;i<8; i++){
-                printf("%d %f\n", j, sA[i]);
-                j++;
-            }
-        }
+//        }
 
         for (int kFragment=0; kFragment<BLOCK_WIDTH; kFragment++) {
 //            if (kBlock==0 and block_idx==0 and thread_id==0) {
