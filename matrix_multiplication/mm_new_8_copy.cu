4f55da12c343c9a2eae2c30380092c68004e8a7e
Steve Siu <steve.siu@oracle.com>
2024-04-30 00:01:59 +1000

    fix

--- matrix_multiplication/mm_new_8_copy.cu
+++ matrix_multiplication/mm_new_8_copy.cu
@@ -95,10 +95,10 @@ void mm_new_8_copy(float* A, float* B, float* C, int N){
 
         for (int kFragment=0; kFragment<BLOCK_WIDTH; kFragment++) {
             // load from smem A, B
-            FLOAT_4(fA[0]) = FLOAT_4(sA(shared_pointer, C_row + kFragment, C_col));
-            FLOAT_4(fA[4]) = FLOAT_4(sA(shared_pointer, C_row + kFragment, C_col + 16));
-            FLOAT_4(fB[0]) = FLOAT_4(sB(shared_pointer, C_row + kFragment, C_col));
-            FLOAT_4(fB[4]) = FLOAT_4(sB(shared_pointer, C_row + kFragment, C_col + 32));
+            FLOAT_4(fA[0]) = FLOAT_4(sA(shared_pointer, (C_row + kFragment), C_col));
+            FLOAT_4(fA[4]) = FLOAT_4(sA(shared_pointer, (C_row + kFragment), C_col + 16));
+            FLOAT_4(fB[0]) = FLOAT_4(sB(shared_pointer, (C_row + kFragment), C_col));
+            FLOAT_4(fB[4]) = FLOAT_4(sB(shared_pointer, (C_row + kFragment), C_col + 32));
 
             // compute outer product
             for (int i=0; i<8;i++){
