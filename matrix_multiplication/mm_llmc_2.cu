203192fd4b9e46bf0aeb8d37ad47924e977b6484
Steve Siu <steve.siu@oracle.com>
2024-05-21 22:41:54 +1000

    fix

--- matrix_multiplication/mm_llmc_2.cu
+++ matrix_multiplication/mm_llmc_2.cu
@@ -81,7 +81,7 @@ void mm_llmc_2(float* A, float* B, float* C, int N){
         sB(pointer, sA_col_sB_row + i, sA_row_sB_col) = rB[i];
     }
 
-    __syncthreads;
+    __syncthreads();
 
     A += BLOCK_WIDTH;
     B += BLOCK_WIDTH;
