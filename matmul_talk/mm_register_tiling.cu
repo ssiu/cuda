0cadd9ea874ee8b8f2704dc74d69e10b9cb46bf0
Steve Siu <steve.siu@oracle.com>
2024-07-17 22:46:52 +1000

    fix

---
+++ matmul_talk/mm_register_tiling.cu
@@ -0,0 +1,70 @@
+// matrix multiplication
+// global memory coalescing
+// shared memory blocking
+// thread coarsening + vectorized memory access
+// each thread loads a row of 4 float and compute a row of 4 floats in C
+// we will use 256 threads dimBlock(8, 32);
+
+//    float4 tmp = reinterpret_cast<float4 *>(&A[innerRowA * K + innerColA * 4])[0];
+//    As[(innerColA * 4 + 0) * BM + innerRowA] = tmp.x;
+//    As[(innerColA * 4 + 1) * BM + innerRowA] = tmp.y;
+//    As[(innerColA * 4 + 2) * BM + innerRowA] = tmp.z;
+//    As[(innerColA * 4 + 3) * BM + innerRowA] = tmp.w;
+#include <iostream>
+
+#define TILE_WIDTH 32
+__global__ void mm_register_tiling_kernel(float* A, float* B, float* C, int N){
+    // offset in output C matrix
+    int gRow_C = threadIdx.y + blockDim.y * blockIdx.y;
+    int gCol_C = threadIdx.x + blockDim.x * blockIdx.x;
+
+    // offset in shared memory;
+    int sRow = threadIdx.y;
+    int sCol = threadIdx.x;
+
+    int gRow_A = gRow_C;
+    int gCol_A;
+    int gRow_B;
+    int gCol_B = gCol_C;
+
+    __shared__ float sA[TILE_WIDTH*TILE_WIDTH];
+    __shared__ float sB[TILE_WIDTH*TILE_WIDTH];
+
+    float sum[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+    //float sum[4] = {};
+    for (int kTile=0; kTile < N/TILE_WIDTH; kTile++){
+        //offset is row, kTile*TILE_WIDTH
+        // reinterpret_cast<float2*>(d_out)[i]
+        gCol_A = kTile*TILE_WIDTH / 4 + threadIdx.x;
+        gRow_B = kTile*TILE_WIDTH + threadIdx.y;
+        // bank conflict free G->S
+        reinterpret_cast<float4*>(sA)[sRow * TILE_WIDTH / 4 + sCol] = reinterpret_cast<float4*>(A)[gRow_A * N / 4 + gCol_A];
+        reinterpret_cast<float4*>(sB)[sRow * TILE_WIDTH / 4 + sCol] = reinterpret_cast<float4*>(B)[gRow_B * N / 4 + gCol_B];
+
+        __syncthreads();
+
+        for (int i=0; i<TILE_WIDTH; i++){
+            #pragma unroll
+            for (int j=0; j<4; j++) {
+//                if (blockIdx.x == 0 and blockIdx.y == 0 and threadIdx.x==0 and threadIdx.y==0){
+//                    printf("SUM[0] is %f\n", sum[j]);
+//                }
+                sum[j] += sA[sRow*TILE_WIDTH + i] * sB[i * TILE_WIDTH + sCol * 4 + j];
+            }
+        }
+        __syncthreads();
+    }
+//    if (blockIdx.x == 0 and blockIdx.x==0 and threadIdx.x==0 and threadIdx.y==0){
+//        printf("SUM[1] is %f\n", sum[1]);
+//    }
+    reinterpret_cast<float4*>(C)[gRow_C * N / 4 + gCol_C] = reinterpret_cast<float4*>(sum)[0];
+
+}
+
+void mm_register_tiling(float* A, float* B, float* C, int N) {
+    dim3 dimGrid3( N / TILE_WIDTH , N / TILE_WIDTH);
+    dim3 dimBlock3(8, 32);
+    mm_register_tiling_kernel<<<dimGrid3, dimBlock3>>>(A, B, C N);
+
+}
