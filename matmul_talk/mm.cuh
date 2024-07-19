65de4b01c1518ebbfb2e74da033cfb2b9ad0b332
Steve Siu <steve.siu@oracle.com>
2024-07-19 22:14:44 +1000

    fix

--- matmul_talk/mm.cuh
+++ matmul_talk/mm.cuh
@@ -9,6 +9,7 @@ void mm_register_tiling(float* A, float* B, float* C, int N);
 void mm_warp_tiling(float* A, float* B, float* C, int N);
 void mm_vectorized_memory_access(float* A, float* B, float* C, int N);
 void mm_shared_memory_bank_conflicts(float* A, float* B, float* C, int N);
+void mm_shared_memory_bank_conflicts_new(float* A, float* B, float* C, int N);
 void mm_double_buffering(float* A, float* B, float* C, int N);
 
 #endif
