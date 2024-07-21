589e14ad5ad0c32a903d048988725900612936df
Steve Siu <steve.siu@oracle.com>
2024-07-21 15:02:01 +1000

    fix

--- matmul_talk/launch_mm.cu
+++ matmul_talk/launch_mm.cu
@@ -36,6 +36,9 @@ int main(){
 //    mm_double_buffering(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
     mm_cublas(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
 
+    hC = dC;
+    hC_cublas = dC_cublas;
+
 
 
     #if 1
