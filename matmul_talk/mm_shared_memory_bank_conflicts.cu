62c9d8aeaece7a2bda629cd3ac74a808d896f715
Steve Siu <steve.siu@oracle.com>
2024-07-21 20:32:14 +1000

    fix

--- matmul_talk/mm_shared_memory_bank_conflicts.cu
+++ matmul_talk/mm_shared_memory_bank_conflicts.cu
@@ -63,23 +63,6 @@ void mm_shared_memory_bank_conflicts_kernel(float* A, float* B, float* C, int N)
 
         __syncthreads();
 
-        if (block_idx == 0 and block_idy == 0 and thread_id == 0 and kBlock == 0) {
-            for (int i = 0; i < 32; i++) {
-                if (i == 16) {
-                    printf("\n");
-                }
-                for (int j = 0; j < 8; j++) {
-                    if (j  == 4 ) {
-                        printf("  ");
-                    }
-                    printf("%f ", sA(j,i));
-                }
-                printf("\n");
-            }
-            printf("\n\n\n");
-        }
-
-
         #pragma unroll
         for (int kFragment=0; kFragment<BLOCK_WIDTH; kFragment++) {
             // load from smem A, B
