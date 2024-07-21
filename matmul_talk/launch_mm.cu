0e83361fcc18563bae79bd1e8d94342d867713df
Steve Siu <steve.siu@oracle.com>
2024-07-21 15:44:23 +1000

    fix

--- matmul_talk/launch_mm.cu
+++ matmul_talk/launch_mm.cu
@@ -31,8 +31,8 @@ int main(){
 //    mm_warp_tiling(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
 //
 //   mm_vectorized_memory_access(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
-    mm_shared_memory_bank_conflicts(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
-//    mm_shared_memory_bank_conflicts_new(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
+//    mm_shared_memory_bank_conflicts(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
+    mm_shared_memory_bank_conflicts_new(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
 //    mm_double_buffering(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC.data()), N);
     mm_cublas(thrust::raw_pointer_cast(dA.data()), thrust::raw_pointer_cast(dB.data()), thrust::raw_pointer_cast(dC_cublas.data()), N);
 
