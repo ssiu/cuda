990ce66e6f8f9d6a6e5bee68a65d2616661baf90
Steve Siu <steve.siu@oracle.com>
2024-07-28 20:26:33 +1000

    fix

--- sum/sum_cub.cu
+++ sum/sum_cub.cu
@@ -3,14 +3,18 @@
 
 
 void sum_cub(float* d_in, float* d_out, int N) {
-    // First call: Determine temporary device storage requirements
+
+    void *d_temp_storage = nullptr;
     size_t temp_storage_bytes = 0;
+
     cub::DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_out, N);
 
-    // Allocate temporary storage
-    void *d_temp_storage = nullptr;
+
+
     cudaMalloc(&d_temp_storage, temp_storage_bytes);
 
-    // Second call: Perform algorithm
+
     cub::DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_out, N);
+    cudaFree(d_temp_storage);
+
 }
