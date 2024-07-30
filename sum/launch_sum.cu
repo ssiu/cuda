077e6e4fe9509a69854782128fa137415b6c06c8
Steve Siu <steve.siu@oracle.com>
2024-07-30 20:38:15 +1000

    fix

--- sum/launch_sum.cu
+++ sum/launch_sum.cu
@@ -23,53 +23,15 @@ int main(){
     thrust::device_vector<float> d_out(1);
     thrust::device_vector<float> d_out_cub(1);
 
+
+    sum_naive(thrust::raw_pointer_cast(d_in.data()), thrust::raw_pointer_cast(d_out_cub.data()), N);
+    h_out[0] = d_out[0];
+    std::cout << h_out[0] << std::endl;
+
     sum_cub(thrust::raw_pointer_cast(d_in.data()), thrust::raw_pointer_cast(d_out_cub.data()), N);
-
-
     h_out_cub[0] = d_out_cub[0];
-//    for (int i=0;i<N;i++) {
-//        std::cout << h_in[i] << " " << std::endl;
-//    }
-
     std::cout << h_out_cub[0] << std::endl;
 
-    #if 0
-        if (isSameMatrices(hC.data(), hC_cublas.data(), N)==0){
-//        for (int i=0;i<N;i += 128){
-//            for (int j=0;j<N; j+=128){
-//                std::cout << N * i + j << " " << hC[N * i + j] << " " << hC_cublas[N * i + j] << std::endl;
-//            }
-//        }
-
-//            for (int i=0;i<100;i++){
-//                std::cout << i << " " << hC[i] << " " << hC_cublas[i] << std::endl;
-//            }
-        for (int i=0;i<32;i++){
-            for (int j=0;j<32;j++){
-                std::cout << hC[i + N*j] << " ";
-            }
-            std::cout << "\n";
-        }
-
-        std::cout << "\n";
-        std::cout << "\n";
-        std::cout << "\n";
-        std::cout << "\n";
-        std::cout << "\n";
-
-        for (int i=0;i<32;i++){
-            for (int j=0;j<32;j++){
-                std::cout << hC_cublas[i + N*j] << " ";
-            }
-            std::cout << "\n";
-        }
-        //        int num = countZeros(hC.data(), N);
-        //        std::cout << "number of zeros in hC is " << num << std::endl;
-            std::cout << "Wrong answer" << std::endl;
-        } else {
-            std::cout << "Correct answer" << std::endl;
-        }
-    #endif
 
 
     return 0;
--- sum/sum.cuh
+++ sum/sum.cuh
@@ -1,6 +1,8 @@
 #ifndef SUM_KERNELS
 #define SUM_KERNELS
 
+
+void sum_naive(float* d_in, float* d_out, int N);
 void sum_cub(float* d_in, float* d_out, int N);
 
 #endif
