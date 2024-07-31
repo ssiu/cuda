c79c6521940a858f5c30b2cf37d2bd70e27c35d7
Steve Siu <steve.siu@oracle.com>
2024-07-31 22:27:10 +1000

    fix

--- sum/sum.cuh
+++ sum/sum.cuh
@@ -4,6 +4,7 @@
 
 void sum_naive(float* d_in, float* d_out, int N);
 void sum_vectorized(float* d_in, float* d_out, int N);
+void sum_atomic_add(float* d_in, float* d_out, int N);
 void sum_cub(float* d_in, float* d_out, int N);
 
 #endif
