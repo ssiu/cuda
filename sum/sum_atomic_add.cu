75edd7484c36a01b8dec39fa946366619b5ddb14
Steve Siu <steve.siu@oracle.com>
2024-07-31 22:37:09 +1000

    fix

--- sum/sum_atomic_add.cu
+++ sum/sum_atomic_add.cu
@@ -40,12 +40,13 @@ __global__ void sum_atomic_add_kernel(float* d_in, float* d_out, int N){
         printf("%f\n", accum[0]);
     }
     if (thread_id == 0) {
-        atomicAdd(d_out[0], accum[0]);
+        atomicAdd(&d_out[0], accum[0]);
     }
 }
 
 void sum_atomic_add(float* d_in, float* d_out, int N) {
     dim3 dimGrid(N / 1024);
+    std::cout << N / 1024 << std::endl;
     dim3 dimBlock(1024);
     sum_atomic_add_kernel<<<dimGrid, dimBlock>>>(d_in, d_out, N);
 }
