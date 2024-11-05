aa5df5d71a7ef75f0770aaaf903a5f694321155b
steve <steve@steves-MacBook-Pro.local>
2024-11-05 13:30:37 +1100

    add sm75 gemm

--- cutlass/sm75_gemm_16x8x8.cu
+++ cutlass/sm75_gemm_16x8x8.cu
@@ -12,48 +12,55 @@
 #include "cutlass/util/GPU_Clock.hpp"
 #include "cutlass/util/helper_cuda.hpp"
 
+using namespace cute;
+
+__global__ void mm_kernel(half_t* A, half_t* B, float* C){
+    if (threadIdx.x ==0) {
+        printf("%f\n", static_cast<float>(A[0]));
+    }
+}
 
 
+void mm(half_t* A, half_t* B, float* C) {
+
+    dim3 dimGrid(1);
+    dim3 dimBlock(32);
+    mm_kernel<<<dimGrid, dimBlock>>>(A, B, C);
+}
 
 int main(int argc, char** argv)
 {
-  int m = 16;
-  int n = 8;
-  int k = 8;
+    int m = 16;
+    int n = 8;
+    int k = 8;
 
-  using TA = cute::half_t;
-  using TB = cute::half_t;
-  using TC = cute::half_t;
+    using TA = half_t;
+    using TB = half_t;
+    using TC = float;
 
-  cute::device_init(0);
+    cute::device_init(0);
 
-  thrust::host_vector<TA> h_A(m*k);
-  thrust::host_vector<TB> h_B(n*k);
-  thrust::host_vector<TC> h_C(m*n);
+    thrust::host_vector<TA> h_A(m*k);
+    thrust::host_vector<TB> h_B(n*k);
+    thrust::host_vector<TC> h_C(m*n);
 
-  for (int j = 0; j < m*k; ++j) {
-      h_A[j] = static_cast<TA>( 2*(rand() / double(RAND_MAX)) - 1 );
-      printf("%f\n", static_cast<float>(h_A[j]));
-  }
-  for (int j = 0; j < n*k; ++j) h_B[j] = static_cast<TB>( 2*(rand() / double(RAND_MAX)) - 1 );
-  for (int j = 0; j < m*n; ++j) h_C[j] = static_cast<TC>(-1);
+    for (int j = 0; j < m*k; ++j) {
+        h_A[j] = static_cast<TA>( 2*(rand() / double(RAND_MAX)) - 1 );
+        printf("%f\n", static_cast<float>(h_A[j]));
+    }
+    for (int j = 0; j < n*k; ++j) h_B[j] = static_cast<TB>( 2*(rand() / double(RAND_MAX)) - 1 );
+    for (int j = 0; j < m*n; ++j) h_C[j] = static_cast<TC>(-1);
 
-  thrust::device_vector<TA> d_A = h_A;
-  thrust::device_vector<TB> d_B = h_B;
-  thrust::device_vector<TC> d_C = h_C;
+    thrust::device_vector<TA> d_A = h_A;
+    thrust::device_vector<TB> d_B = h_B;
+    thrust::device_vector<TC> d_C = h_C;
 
-//   int ldA = m, ldB = n, ldC = m;
-//
-//   // Run once
-//   d_C = h_C;
-//   matmul(
-//        d_A.data().get(), ldA,
-//        d_B.data().get(), ldB,
-//        d_C.data().get(), ldC);
-//   CUTE_CHECK_LAST();
-//   thrust::host_vector<TC> cute_result = d_C;
+
+    mm(d_A.data().get(), d_B.data().get(), d_C.data().get());
+
+    thrust::host_vector<TC> cute_result = d_C;
 
 
 
-  return 0;
+    return 0;
 }
