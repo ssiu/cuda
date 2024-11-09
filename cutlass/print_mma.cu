cc83a6e06e843d34f17ff284233638971b1cb903
steve <stevesiu1013@gmail.com>
2024-11-09 20:13:01 +1100

    fix

--- cutlass/print_mma.cu
+++ cutlass/print_mma.cu
@@ -10,7 +10,7 @@ int main() {
     }
     #endif
 
-    #if 1
+    #if 0
     {
         auto tiled_mma = make_tiled_mma(SM75_16x8x8_F32F16F16F32_TN{},
                             Layout<Shape<_1, _2, _1>>{},
@@ -20,6 +20,13 @@ int main() {
     }
     #endif
 
+    #if 1
+    {
+        auto tiled_mma = make_tiled_mma(SM75_16x8x8_F32F16F16F32_TN{},
+                            Layout<Shape<_2, _4, _1>>{});
+        print_latex(tiled_mma);
+    }
+    #endif
 
     #if 0
     {
