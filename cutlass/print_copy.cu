fff7a5c01b00013e635b1d21490b97b7d894012b
steve <stevesiu1013@gmail.com>
2024-11-14 12:15:35 +1100

    fix

--- cutlass/print_copy.cu
+++ cutlass/print_copy.cu
@@ -110,8 +110,8 @@ int main()
     SmemCopyB smem_copy_b;
     SmemLayoutA smem_layout_a;
 
-    //print_latex(smem_layout_a);
-    print_latex(gmem_copy_a);
+    print_latex(smem_layout_a);
+    //print_latex(gmem_copy_a);
     //print_latex(smem_copy_a);
     //print_latex(smem_copy_a);
 
