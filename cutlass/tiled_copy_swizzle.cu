74b2eaf455ae69688bc0318ade2cc33286ae75e4
steve <stevesiu1013@gmail.com>
2024-11-16 15:57:20 +1100

    fix

--- cutlass/tiled_copy_swizzle.cu
+++ cutlass/tiled_copy_swizzle.cu
@@ -47,7 +47,7 @@ void mm(T* in, T* out) {
                         make_stride(Int<8>{}, Int<1>{}));
 
     auto out_layout = make_layout(make_shape (Int<8>{}, Int<8>{}),
-                        make_stride(Int<1>{}, Int<8>{}));
+                        make_stride(Int<8>{}, Int<1>{}));
 
     auto smem_layout = make_layout(make_shape (Int<8>{}, Int<8>{}),
                                                make_stride(Int<1>{}, Int<8>{}));
