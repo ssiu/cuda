56c5546a9667b9325149f0b973d6dbfb1eb4deb7
steve <stevesiu1013@gmail.com>
2024-11-16 20:57:51 +1100

    fix

--- cutlass/tiled_copy_swizzle.cu
+++ cutlass/tiled_copy_swizzle.cu
@@ -100,7 +100,7 @@ int main(int argc, char** argv)
     thrust::device_vector<T> d_in = h_in;
     thrust::device_vector<T> d_out = h_out;
 
-    mm(d_in.data().get(), d_out.data().get());
+    //mm(d_in.data().get(), d_out.data().get());
 
     h_out = d_out;
 
