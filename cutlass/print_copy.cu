2a74d7249915203a4cd955c421571257ba0146cd
steve <stevesiu1013@gmail.com>
2024-11-18 20:20:40 +1100

    fix

--- cutlass/print_copy.cu
+++ cutlass/print_copy.cu
@@ -7,7 +7,7 @@ int main()
 #if 1
 
         auto mma = make_tiled_mma(SM75_16x8x8_F32F16F16F32_TN{}, Layout<Shape<_1, _1, _1>>{}, Tile<_32,_32,_8>{});
-        auto tiled_copy = make_tiled_copy_A(Copy_Atom<SM75_U32x4_LDSM_N, half_t>{}, mma);
+        auto tiled_copy = make_tiled_copy_B(Copy_Atom<SM75_U32x4_LDSM_N, half_t>{}, mma);
 
         print_latex(tiled_copy);
 #endif
