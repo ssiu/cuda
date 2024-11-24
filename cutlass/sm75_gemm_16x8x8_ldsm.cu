60b1462adee615572ae2f120fb969dbccedd37e7
steve <stevesiu1013@gmail.com>
2024-11-24 23:48:00 +1100

    fix

--- cutlass/sm75_gemm_16x8x8_ldsm.cu
+++ cutlass/sm75_gemm_16x8x8_ldsm.cu
@@ -54,12 +54,12 @@ __global__ void mm_kernel(
     auto s2r_tiled_copy_a = make_tiled_copy_A(Copy_Atom<SM75_U32x1_LDSM_N, half_t>{}, mma);
     auto s2r_thr_copy_a = s2r_tiled_copy_a.get_slice(threadIdx.x);
     auto s2r_tAsA = s2r_thr_copy_a.partition_S(sA);
-    //auto tCrA_copy_view = s2r_thr_copy_a.retile_D(tCrA);
+    auto tCrA_copy_view = s2r_thr_copy_a.retile_D(tCrA);
 
     auto s2r_tiled_copy_b = make_tiled_copy_B(Copy_Atom<SM75_U32x1_LDSM_N, half_t>{}, mma);
     auto s2r_thr_copy_b = s2r_tiled_copy_b.get_slice(threadIdx.x);
     auto s2r_tBsB = s2r_thr_copy_b.partition_S(sB);
-    //auto tCrB_copy_view = s2r_thr_copy_b.retile_D(tCrB);
+    auto tCrB_copy_view = s2r_thr_copy_b.retile_D(tCrB);
 
 
     //printf("tCrC: %f\n", tCrC[0]);
@@ -74,7 +74,14 @@ __global__ void mm_kernel(
         print_tensor(sA);
         print_tensor(sB);
     }
-
+    if (thread0()) {
+//         print_tensor(sA);
+//         print_tensor(sB);
+        print("tCrA           : "); print(  tCrA); print("\n");
+        print("tCrA_copy_view : "); print(  tCrA_copy_view); print("\n");
+        print("tCrB           : "); print(tCrB); print("\n");
+        print("tCrB_copy_view : "); print(tCrB_copy_view); print("\n");
+    }
 
 //     copy(s2r_tiled_copy_a, s2r_tAsA, tCrA_copy_view);
 //     copy(s2r_tiled_copy_b, s2r_tBsB, tCrB_copy_view);
