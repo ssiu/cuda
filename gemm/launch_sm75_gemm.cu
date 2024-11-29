daab15c4f964cda94461102a619a93fdfd54ed02
steve <stevesiu1013@gmail.com>
2024-11-30 08:53:26 +1100

    fix

--- gemm/launch_sm75_gemm.cu
+++ gemm/launch_sm75_gemm.cu
@@ -27,8 +27,8 @@ int main(int argc, char** argv)
 
     cute::device_init(0);
 
-    thrust::host_vector<TA> h_A(m*k) = generateRandomMatrix<TA> (m * k);
-    thrust::host_vector<TB> h_B(n*k) = generateRandomMatrix<TB> (n * k);
+    thrust::host_vector<TA> h_A = generateRandomMatrix<TA> (m * k);
+    thrust::host_vector<TB> h_B = generateRandomMatrix<TB> (n * k);
     thrust::host_vector<TC> h_C(m*n);
     thrust::host_vector<TC> h_C_cublas = generateRandomMatrix<TC>(m * n);
 
