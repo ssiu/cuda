310b5d308a2e66da58a3c48f44c5d2f42690d30c
steve <stevesiu1013@gmail.com>
2024-12-03 00:24:37 +1100

    fix

--- gemm/benchmark.sh
+++ gemm/benchmark.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-output_csv="compute_throughput.csv"
-echo "Size,Compute_Throughput" > $output_csv
-
 # Array of values to loop through
 sizes=(512 1024 2048 4096 8192)
 
 for size in "${sizes[@]}"; do
     echo "Running with size $size..."
-    throughput=$(ncu --metrics sm__throughput.avg.pct_of_peak_sustained_elapsed_time \
-                     --set full \
-                     --csv ./launch_sm75_gemm.o $size $size $size\
-                     | grep "sm__throughput.avg.pct_of_peak_sustained_elapsed_time" \
-                     | awk -F ',' '{print $2}')
-    echo "$size,$throughput" >> $output_csv
+    ncu --metrics sm__throughput.avg.pct_of_peak_sustained_elapsed \
+        --csv ./launch_sm75_gemm.o $size $size $size > $(size).csv
+
 done
