482693af9348b3de18687dfab5b50eb924881c96
steve <stevesiu1013@gmail.com>
2024-12-03 00:28:42 +1100

    fix

--- gemm/benchmark.sh
+++ gemm/benchmark.sh
@@ -6,6 +6,8 @@ sizes=(512 1024 2048 4096 8192)
 for size in "${sizes[@]}"; do
     echo "Running with size $size..."
     ncu --metrics sm__throughput.avg.pct_of_peak_sustained_elapsed \
-        --csv ./launch_sm75_gemm.o $size $size $size > $(size).csv
+        --csv ./launch_sm75_gemm.o "$size" "$size" "$size" > "${size}.csv"
+
+    echo "Output saved to ${size}.csv"
 
 done
