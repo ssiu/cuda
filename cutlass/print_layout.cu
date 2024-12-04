98fe54e212d8684ceff5d40ab12d261d913c9883
steve <stevesiu1013@gmail.com>
2024-12-04 22:45:13 +1100

    fix

--- cutlass/print_layout.cu
+++ cutlass/print_layout.cu
@@ -17,9 +17,9 @@ int main() {
     {
 //        auto smem_atom = composition(Swizzle<2,0,3>{}, Layout<Shape<_4,_8>,Stride<_8,_1>>{});
 //        print_layout(smem_atom);
-        auto layout = composition(Swizzle<1, 3, 1>{},
-                                    Layout<Shape<_8, _8>,
-                                    Stride<_8, _1>>{});
+        auto layout = composition(Swizzle<1, 2, 1>{},
+                                    Layout<Shape<_16, _4>,
+                                    Stride<_4, _1>>{});
         print_layout(layout);
     }
     #endif
