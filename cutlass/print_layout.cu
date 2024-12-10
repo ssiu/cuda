dad0a8ef4a3bdd8b315ccc2a9734029a259020b7
steve <stevesiu1013@gmail.com>
2024-12-10 21:24:05 +1100

    fix

--- cutlass/print_layout.cu
+++ cutlass/print_layout.cu
@@ -9,13 +9,19 @@ int main() {
     {
 //        auto smem_atom = composition(Swizzle<2,0,3>{}, Layout<Shape<_4,_8>,Stride<_8,_1>>{});
 //        print_layout(smem_atom);
-
-        auto SmemLayoutAtom = decltype(composition(
-                                Swizzle<3, 3, 3>{},
-                                make_layout(make_shape(Int<32>{}, Int<8>{}),
-                                            make_stride(Int<1>{}, Int<128>{}))));
-
-        auto SmemLayout = tile_to_shape(SmemLayoutAtom{}, make_shape(Int<128>{}, Int<32>{}));
+        using SmemLayoutAtom = decltype(composition(
+            Swizzle<3, 3, 3>{},
+            make_layout(make_shape(Int<32>{}, Int<8>{}),
+                        make_stride(Int<1>{}, Int<128>{}))));
+        using SmemLayoutA = decltype(tile_to_shape(SmemLayoutAtom{},
+                                                   make_shape(Int<128>{}, Int<32>{})));
+        SmemLayoutA SmemLayout;
+//         auto SmemLayoutAtom = decltype(composition(
+//                                 Swizzle<3, 3, 3>{},
+//                                 make_layout(make_shape(Int<32>{}, Int<8>{}),
+//                                             make_stride(Int<1>{}, Int<128>{}))));
+//
+//         auto SmemLayout = tile_to_shape(SmemLayoutAtom{}, make_shape(Int<128>{}, Int<32>{}));
 
         print_layout(SmemLayout);
 
