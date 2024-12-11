a6ec90c344462c26bd096a531f19c5e437014bb0
steve <stevesiu1013@gmail.com>
2024-12-11 21:36:43 +1100

    fix

--- gemm/sm75_gemm_vectorized_gmem_store_256.cu
+++ gemm/sm75_gemm_vectorized_gmem_store_256.cu
@@ -41,7 +41,7 @@ __global__ void gemm_vectorized_gmem_store_256_kernel(
 
     Tensor sA = make_tensor(make_smem_ptr(smemA), sA_layout);
     Tensor sB = make_tensor(make_smem_ptr(smemB), sB_layout);
-    Tensor sB = make_tensor(make_smem_ptr(smemC), sC_layout);
+    Tensor sC = make_tensor(make_smem_ptr(smemC), sC_layout);
 
     ThrCopy thr_copy_a = copy_a.get_slice(threadIdx.x);
     Tensor tAgA = thr_copy_a.partition_S(gA);                            // (CPY,CPY_M,CPY_K,k)
