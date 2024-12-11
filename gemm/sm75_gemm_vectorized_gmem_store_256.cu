032f69ff17ca4eb5af7b64f3309e528a41f9f0c0
steve <stevesiu1013@gmail.com>
2024-12-12 09:44:22 +1100

    fix

--- gemm/sm75_gemm_vectorized_gmem_store_256.cu
+++ gemm/sm75_gemm_vectorized_gmem_store_256.cu
@@ -113,13 +113,13 @@ __global__ void gemm_vectorized_gmem_store_256_kernel(
     }
 
     //axpby(1.0f, tCrC, 0.0f, tCgC); //vectorized_load
-   copy(tCrC, tCgC);
-//    copy(tCrC, tCsC);
-//    __syncthreads();
+//   copy(tCrC, tCgC);
+   copy(tCrC, tCsC);
+   __syncthreads();
 //    copy(tCsC, tCgC);
 //     __syncthreads();
 //
-//     copy(copy_c, s2g_tCsC, s2g_tCgC);
+    copy(copy_c, s2g_tCsC, s2g_tCgC);
     
     #if 0
         if(thread0()) {
