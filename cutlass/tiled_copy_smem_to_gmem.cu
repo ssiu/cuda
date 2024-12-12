a895fecd56d482b2abdc977b3ff3649516cb87a2
steve <stevesiu1013@gmail.com>
2024-12-12 12:36:49 +1100

    fix

--- cutlass/tiled_copy_smem_to_gmem.cu
+++ cutlass/tiled_copy_smem_to_gmem.cu
@@ -23,7 +23,7 @@ __global__ void mm_kernel(
     Tensor g_in = make_tensor(make_gmem_ptr(in), layout_in);
     Tensor g_out = make_tensor(make_gmem_ptr(out), layout_out);
 
-//     __shared__ T smem[cosize_v<LayoutOut>];
+    __shared__ T smem[cosize_v<LayoutOut>];
 
     ThrCopy thr_copy = tiled_copy.get_slice(threadIdx.x);
     Tensor tg_in = thr_copy.partition_S(g_in);
