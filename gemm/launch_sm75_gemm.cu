841eb0f30b5491f7b3f5ca8928752df90485c9b5
steve <stevesiu1013@gmail.com>
2024-12-17 21:09:55 +1100

    fix

--- gemm/launch_sm75_gemm.cu
+++ gemm/launch_sm75_gemm.cu
@@ -27,7 +27,7 @@
 #include "sm75_gemm_register_pipelining_128.cu"
 #include "sm75_gemm_register_pipelining_128_bk_64.cu"
 #include "sm75_gemm_vectorized_store_128.cu"
-
+#include "sm75_gemm_vectorized_store_256.cu"
 using namespace cute;
 
 
@@ -147,6 +147,8 @@ int main(int argc, char** argv)
 //     for (int i=0;i<100;i++) {
 //         print("cublas = %f, gemm = %f\n", h_C_cublas[i], h_C[i]);
 //     }
-
+    gemm_vectorized_store_256(d_A.data().get(), d_B.data().get(), d_C.data().get(), m, n, k);
+    h_C = d_C;
+    isSameMatrices(h_C.data(), h_C_cublas.data(), m * n, "vectorized_store_256");
     return 0;
 }
