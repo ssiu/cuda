cca280d4cacd69ed7c14cb4681a5a4042f55223b
steve <stevesiu1013@gmail.com>
2024-12-26 16:37:47 +1100

    fix

--- gemm/launch_sm75_gemm.cu
+++ gemm/launch_sm75_gemm.cu
@@ -132,7 +132,7 @@ int main(int argc, char** argv)
 
     gemm_register_pipelining_new_256(d_A.data().get(), d_B.data().get(), d_C.data().get(), m, n, k);
     h_C = d_C;
-    isSameMatrices(h_C.data(), h_C_cublas.data(), m * n, "register_pipelining_256");
+    isSameMatrices(h_C.data(), h_C_cublas.data(), m * n, "register_pipelining_new_256");
 
     gemm_vectorized_gmem_store_256(d_A.data().get(), d_B.data().get(), d_C.data().get(), m, n, k);
     h_C = d_C;
