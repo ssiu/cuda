3fb63c6e5345b1f3a0ecbd532f0102ddb323b819
steve <stevesiu1013@gmail.com>
2025-01-16 23:53:25 +1100

    fix

--- cutlass/tensor_op.cu
+++ cutlass/tensor_op.cu
@@ -6,6 +6,7 @@ int main()
 {
     int A[8] = {1, 2, 3, 4, 5, 6, 7, 8};
     Tensor a = make_tensor(&A[0], make_shape(Int<2>{}, Int<2>{}, Int<2>{}), make_stride(Int<4>{}, Int<2>{}, Int<1>{}));
+    print_tensor(a);
     print_tensor(a(_,0,_));
 
     Tensor b = local_tile(a(_, 0, _), Shape<Int<1>, Int<2>>{},
