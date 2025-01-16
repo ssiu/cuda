9b96ee0d5596bf42892c2f7cd6cb2c602538d21e
steve <stevesiu1013@gmail.com>
2025-01-16 23:29:18 +1100

    fix

--- cutlass/tensor_op.cu
+++ cutlass/tensor_op.cu
@@ -6,5 +6,5 @@ int main()
 {
     int A[8] = {1, 2, 3, 4, 5, 6, 7, 8};
     Tensor a = make_tensor(&A[0], make_shape(Int<2>{}, Int<2>{}, Int<2>{}));
-    print_tensor(a);
+    print_tensor(a(_,0,_));
 }
