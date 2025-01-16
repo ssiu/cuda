4258c1eaca24dfb2f7117d31e8ea05ac91252c30
steve <stevesiu1013@gmail.com>
2025-01-16 22:04:05 +1100

    fix

--- cutlass/tensor_op.cu
+++ cutlass/tensor_op.cu
@@ -4,7 +4,7 @@ using namespace cute;
 
 int main()
 {
-    float* A[6] = {1,2,3,4,5,6};
+    int* A[6] = {1, 2, 3, 4, 5, 6};
     Tensor a = make_tensor(A, make_shape(Int<3>{},Int<2>{}), make_stride(Int<1>{},Int<3>{});
     print_tensor(a);
 }
