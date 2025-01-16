d37de207e0532f53882d008f7e943cf10df2fb91
steve <stevesiu1013@gmail.com>
2025-01-16 21:53:30 +1100

    added tensor op

---
+++ cutlass/tensor_op.cu
@@ -0,0 +1,10 @@
+#include <cute/layout.hpp>
+#include <cute/tensor.hpp>
+using namespace cute;
+
+int main()
+{
+    float* A[6] = {1,2,3,4,5,6};
+    Tensor a = make_tensor(A, make_shape(Int<3>{},Int<2>{}), make_stride(Int<1>{},Int<3>{});
+    print_tensor(a);
+}
