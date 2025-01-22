e28ae623f0d0b90ba74f8d70e1a34b92b01219bb
steve <stevesiu1013@gmail.com>
2025-01-23 10:54:57 +1100

    fix

--- cutlass/tensor_op.cu
+++ cutlass/tensor_op.cu
@@ -12,7 +12,7 @@ int main()
     Tensor b = local_tile(a(_, 0, _), Shape<Int<1>, Int<2>>{},
                           make_coord(_, 0));  // (1, 2, 2)
     print_tensor(b);
-    printf("%d", (b(0,0,1));
+    printf("%d\n", b(0,0,1));
 
 
 }
