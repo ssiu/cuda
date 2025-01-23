e990f6c512e02a57051dfbd0386cda8a69d51dc0
steve <stevesiu1013@gmail.com>
2025-01-23 11:01:41 +1100

    fix

--- cutlass/tensor_op.cu
+++ cutlass/tensor_op.cu
@@ -13,6 +13,7 @@ int main()
                           make_coord(_, 0));  // (1, 2, 2)
     print_tensor(b);
     printf("%d %d %d\n", b(0,0,1), b(0,1,0), b(0,0,1) + b(0,1,0));
-
+    b(0,0,1) = 10;
+    print_tensor(b);
 
 }
