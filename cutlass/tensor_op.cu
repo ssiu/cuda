b6f371038a77a3ef05184454aec98402ebb3b0ac
steve <stevesiu1013@gmail.com>
2025-01-23 16:47:34 +1100

    fix

--- cutlass/tensor_op.cu
+++ cutlass/tensor_op.cu
@@ -9,11 +9,14 @@ int main()
     print_tensor(a); // (2, 2, 2)
     print_tensor(a(_,0,_)); // (2, 2)
 
-    Tensor b = local_tile(a(_, 0, _), Shape<Int<1>, Int<2>>{},
-                          make_coord(_, 0));  // (1, 2, 2)
-    print_tensor(b);
-    printf("%d %d %d\n", b(0,0,1), b(0,1,0), b(0,0,1) + b(0,1,0));
-    b(0,0,1) = 10;
-    print_tensor(b);
+    Tensor b = make_tensor(&A[0], make_shape(Int<4>{}, Int<3>{}, Int<2>{}), make_stride(Int<1>{}, Int<2>{}, Int<6>{}));
+    print_tensor(b); // (2, 2, 2)
+    print_tensor(b(_,0,_)); // (2, 2)
+//     Tensor b = local_tile(a(_, 0, _), Shape<Int<1>, Int<2>>{},
+//                           make_coord(_, 0));  // (1, 2, 2)
+//     print_tensor(b);
+//     printf("%d %d %d\n", b(0,0,1), b(0,1,0), b(0,0,1) + b(0,1,0));
+//     b(0,0,1) = 10;
+//     print_tensor(b);
 
 }
