4a7f81c840e32259cd5aa9260d70adbdf3be53d7
steve <stevesiu1013@gmail.com>
2025-02-02 14:37:40 +1100

    fix

--- cpp_extension_test/my_extension.cpp
+++ cpp_extension_test/my_extension.cpp
@@ -1,7 +1,10 @@
 #include <torch/extension.h>
 
 void launch_my_cuda_kernel(torch::Tensor x);
+torch::Tensor mm_new_8(torch::Tensor a, torch::Tensor b);
+
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("cuda_kernel", &launch_my_cuda_kernel, "My CUDA Kernel");
+    m.def("mm_new_8", &mm_new_8, "gemm kernel");
 }
