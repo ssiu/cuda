303621f27e7c108903aeb19537c49bd16f2c8e16
steve <stevesiu1013@gmail.com>
2025-02-02 21:32:54 +1100

    fix

--- cpp_extension_test/setup.py
+++ cpp_extension_test/setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 
+from cpp_extension.setup import nvcc_flags
+
+cutlass_include_dirs = ["/cutlass/include", "/cutlass/tools/util/include"]
+
+nvcc_flags = ["-std=c++17",
+              "--expt-relaxed-constexpr",
+              "-arch=sm_75",
+              "-O3"]
+
 setup(
     name="my_cuda_extension",
     ext_modules=[
         CUDAExtension(
-            "my_cuda_extension",
-            ["my_extension.cpp", "my_kernel.cu"]
+            name="my_cuda_extension",
+            sources=["my_extension.cpp", "my_kernel.cu"],
+            include_dirs=cutlass_include_dirs,
+            extra_compile_args={'nvcc': nvcc_flags}
         )
     ],
     install_requires=["torch"],
