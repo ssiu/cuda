8b65c9ce81c2f633b519c91491f217b723f456d4
steve <stevesiu1013@gmail.com>
2025-02-05 20:35:13 +1100

    fix

--- flash_attn_turing/test.py
+++ flash_attn_turing/test.py
@@ -37,7 +37,7 @@ for i in range(batch_size):
         for k in range(nheads):
             for l in range(headdim):
                 if count < 100:
-                    print(output[i,j,k,l], output_torch[i,j,k,l])
+                    print(count, i, j, k, l, output[i,j,k,l], output_torch[i,j,k,l])
                 lse += (output[i,j,k,l] - output_torch[i,j,k,l])**2
 
 print(lse)
