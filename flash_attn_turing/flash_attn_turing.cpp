bf48ee37a6154d5b0eb4ddfeca56e30001022825
steve <stevesiu1013@gmail.com>
2025-02-07 09:02:08 +1100

    fix

--- flash_attn_turing/flash_attn_turing.cpp
+++ flash_attn_turing/flash_attn_turing.cpp
@@ -1,12 +1,13 @@
 #include <torch/extension.h>
 
-torch::Tensor flash_fwd_v0(torch::Tensor q, torch::Tensor k, torch::Tensor v,
-                            int batch_size, int seq_len, int num_heads, int head_dim);
+//torch::Tensor flash_fwd_v0(torch::Tensor q, torch::Tensor k, torch::Tensor v,
+//                            int batch_size, int seq_len, int num_heads, int head_dim);
+
 torch::Tensor flash_fwd_v1(torch::Tensor q, torch::Tensor k, torch::Tensor v,
                             int batch_size, int seq_len, int num_heads, int head_dim);
 
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("flash_fwd_v0", &flash_fwd_v0, "Flash Attention forward Turing");
-    m.def("flash_fwd_v1", &flash_fwd_v1, "Flash Attention forward Turing");
+    //m.def("flash_fwd_v0", &flash_fwd_v0, "flash fwd v0");
+    m.def("flash_fwd_v1", &flash_fwd_v1, "flash fwd v1");
 }
