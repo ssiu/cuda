8dedf919fe830bd96f2581af797ed218e3ce65ea
steve <stevesiu1013@gmail.com>
2025-02-07 15:01:55 +1100

    fix

--- flash_attn_turing/test.py
+++ flash_attn_turing/test.py
@@ -20,7 +20,7 @@ def get_lse(batch_size=1, seqlen=16, nheads=1, headdim=128):
 
     # (batch_size, nheads, seqlen, headdim)
     output_torch = F.scaled_dot_product_attention(query_torch, key_torch, value_torch)
-
+    lse = 0
     for i in range(batch_size):
         for j in range(seqlen):
             for k in range(nheads):
