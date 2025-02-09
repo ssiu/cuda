961b6710df4c4b5133bab0ee1008ac5ba667067d
steve <stevesiu1013@gmail.com>
2025-02-09 20:10:07 +1100

    fix

--- flash_attn_turing/flash_fwd_v1.cu
+++ flash_attn_turing/flash_fwd_v1.cu
@@ -109,7 +109,7 @@ void flash_fwd_v1_kernel(
     Tensor sV = make_tensor(sK.data() + 16*128, sV_layout);
     Tensor sP = make_tensor(sV.data() + 16*128, sS_layout);
 
-    Tensor sS = make_tensor(make_smem_ptr(reinterpret_cast<float*>(sP.data() + 16*16), sS_layout);
+    Tensor sS = make_tensor(make_smem_ptr(reinterpret_cast<float*>(sP.data() + 16*16)), sS_layout);
     Tensor sP_float = make_tensor(sS.data() + 16*16, sS_layout);
     Tensor sO = make_tensor(sP_float.data() + 16*16, sO_layout);
     Tensor sO_accum = make_tensor(sO.data() + 16*128, sO_layout);
