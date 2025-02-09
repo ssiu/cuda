aaad190c748064422725155fa70bbe6328ab25bc
steve <stevesiu1013@gmail.com>
2025-02-09 15:19:19 +1100

    fix

--- flash_attn_turing/setup.py
+++ flash_attn_turing/setup.py
@@ -16,8 +16,8 @@ setup(
         CUDAExtension(
             name="flash_attn_turing",
             sources=["flash_attn_turing.cpp",
-                     #"flash_fwd_v0.cu",
-                     "flash_fwd_v1.cu"
+                     "flash_fwd_v0.cu",
+                     #"flash_fwd_v1.cu"
                      ],
             include_dirs=cutlass_include_dirs,
             extra_compile_args={'nvcc': nvcc_flags}
