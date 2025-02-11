ff145f51708910cb936be3a0982573594a92f7db
steve <stevesiu1013@gmail.com>
2025-02-11 11:04:59 +1100

    fix

--- flash_attn_turing/benchmark.sh
+++ flash_attn_turing/benchmark.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+export CXX=g++
+export CC=gcc
 
-ncu -f --target-processes all --set full \
---import-source on \
--o profile_flash_attn python test.py
+pip install .
+
+python test.py
+
+#ncu -f --target-processes all --set full \
+#--import-source on \
+#-o profile_flash_attn python test.py
