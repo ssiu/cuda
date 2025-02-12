df4b9cee59c00cb1d49c8b117a4d8f92ecbee65b
steve <stevesiu1013@gmail.com>
2025-02-13 07:46:30 +1100

    fix

--- flash_attn_turing/test.py
+++ flash_attn_turing/test.py
@@ -49,13 +49,13 @@ def get_lse(batch_size=1, seqlen=16, nheads=1, headdim=128):
 
 
 lse_16 = get_lse(batch_size=1, seqlen=16, nheads=1, headdim=128)
-# lse_32 = get_lse(batch_size=1, seqlen=32, nheads=1, headdim=128)
-# lse_1024 = get_lse(batch_size=4, seqlen=1024, nheads=4, headdim=128)
+lse_32 = get_lse(batch_size=1, seqlen=32, nheads=1, headdim=128)
+lse_1024 = get_lse(batch_size=4, seqlen=1024, nheads=4, headdim=128)
 
 
 print(f"lse_16 = {lse_16}")
-# print(f"lse_32 = {lse_32}")
-# print(f"lse_1024 = {lse_1024}")
+print(f"lse_32 = {lse_32}")
+print(f"lse_1024 = {lse_1024}")
 
 
 # #debug
