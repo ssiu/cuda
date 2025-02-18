25accd5257644eaf92d065ebd778cd0077e558fc
steve <stevesiu1013@gmail.com>
2025-02-18 14:53:52 +1100

    fix

--- flash_attn_turing/flash_fwd_v4.cu
+++ flash_attn_turing/flash_fwd_v4.cu
@@ -25,7 +25,7 @@ template <class SmemLayoutQ, class TiledCopyQ, class TiledMmaS,
           class SmemLayoutS,
           class SmemLayoutO, class TiledCopyO>
 __global__ __launch_bounds__(256)
-void flash_fwd_v5_kernel(
+void flash_fwd_v4_kernel(
     half_t const* q, SmemLayoutQ sQ_layout, TiledCopyQ copy_Q, TiledMmaS mma_S,
     half_t const* k, SmemLayoutK sK_layout, TiledCopyK copy_K, TiledMmaO mma_O,
     half_t const* v, SmemLayoutV sV_layout, TiledCopyV copy_V,
@@ -345,7 +345,7 @@ void flash_fwd_v5_kernel(
 // define mQ, mK, mV, mO
 // define gQ, gK, gV, gO
 // how to compute softmax
-torch::Tensor flash_fwd_v5(torch::Tensor q, torch::Tensor k, torch::Tensor v,
+torch::Tensor flash_fwd_v4(torch::Tensor q, torch::Tensor k, torch::Tensor v,
                                 int batch_size, int seq_len, int num_heads, int head_dim)
 {
     //  input : (B, S, NH, HD)
@@ -421,14 +421,14 @@ torch::Tensor flash_fwd_v5(torch::Tensor q, torch::Tensor k, torch::Tensor v,
     int maxbytes = 65536;
 
 
-    auto kernel = flash_fwd_v5_kernel<decltype(sQ_layout), decltype(copy_Q), decltype(mma_S),
+    auto kernel = flash_fwd_v4_kernel<decltype(sQ_layout), decltype(copy_Q), decltype(mma_S),
                                       decltype(sK_layout), decltype(copy_K), decltype(mma_O),
                                       decltype(sV_layout), decltype(copy_V),
                                       decltype(sS_layout),
                                       decltype(sO_layout), decltype(copy_O)>;
 
     cudaFuncSetAttribute(kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, maxbytes);
-    flash_fwd_v5_kernel<<<dimGrid, dimBlock, maxbytes>>>(q_ptr, sQ_layout, copy_Q, mma_S,
+    flash_fwd_v4_kernel<<<dimGrid, dimBlock, maxbytes>>>(q_ptr, sQ_layout, copy_Q, mma_S,
                                                          k_ptr, sK_layout, copy_K, mma_O,
                                                          v_ptr, sV_layout, copy_V,
                                                                 sS_layout,
