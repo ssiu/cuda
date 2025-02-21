3734c216917b9b64b7e264df4068ca38dffa331a
steve <stevesiu1013@gmail.com>
2025-02-21 23:18:43 +1100

    fix

--- cutlass/tensor_mma.cu
+++ cutlass/tensor_mma.cu
@@ -5,22 +5,22 @@ using namespace cute;
 int main()
 {
     // need to understand how to call the elements in a register tensor on partition_C when we tile the tensor core multiple times
-    int A[16*8] = {0};
-    auto layout_A = make_layout(make_shape (Int<16>{}, Int<8>{}),
+    int C[16*16] = {0};
+    auto layout_C = make_layout(make_shape (Int<16>{}, Int<16>{}),
                             make_stride(Int<8>{}, Int<1>{}));
 
-    Tensor a = make_tensor(&A[0], layout_A);
+    Tensor c = make_tensor(&C[0], layout_C);
 
     auto mma = make_tiled_mma(SM75_16x8x8_F32F16F16F32_TN{},
                                 Layout<Shape<_1, _1, _1>>{},
                                 Tile<_16,_16,_8>{});
     ThrMMA thr_mma = mma.get_slice(0);
 
-    Tensor ta = thr_mma.partition_C(a);
+    Tensor tc = thr_mma.partition_C(c);
 
-    print(ta);
-    print_tensor(ta);
-    printf("%d %d %d %d\n", int(size<0,0>(ta)), int(size<0,1>(ta)), int(size<1>(ta)), int(size<2>(ta)));
+    print(tc);
+    print_tensor(tc);
+    printf("%d %d %d %d\n", int(size<0,0>(tc)), int(size<0,1>(tc)), int(size<1>(tc)), int(size<2>(tc)));
 
 
 }
