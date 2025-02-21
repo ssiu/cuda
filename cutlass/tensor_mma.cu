922672e267661b88da51867038e5d35194f41a8b
steve <stevesiu1013@gmail.com>
2025-02-21 22:28:56 +1100

    fix

---
+++ cutlass/tensor_mma.cu
@@ -0,0 +1,18 @@
+
+#include <cute/tensor.hpp>
+using namespace cute;
+
+int main()
+{
+    half_t A[16*8] = {0};
+    auto layout_A = make_layout(make_shape (Int<16>{}, Int<8>{}),
+                            make_stride(Int<8>{}, Int<1>{}));
+
+    Tensor b = make_tensor(&A[0], layout_A);
+
+    auto mma = make_tiled_mma(SM75_16x8x8_F32F16F16F32_TN{});
+    ThrMMA thr_mma_O = mma.get_slice(0);
+
+
+
+}
