c08b7d5c8d3d73e1989c0d186af9ec01a8af6fb3
steve <stevesiu1013@gmail.com>
2025-02-22 08:28:48 +1100

    fix

--- cutlass/tensor_mma.cu
+++ cutlass/tensor_mma.cu
@@ -4,8 +4,9 @@ using namespace cute;
 
 int main()
 {
+
     // need to understand how to call the elements in a register tensor on partition_C when we tile the tensor core multiple times
-    int C[16*16] = {0};
+    int C[16*32] = {0};
     for (int i = 0; i < 16*16; ++i) {
         C[i] = i;
     }
@@ -18,7 +19,7 @@ int main()
 
     auto mma = make_tiled_mma(SM75_16x8x8_F32F16F16F32_TN{},
                                 Layout<Shape<_1, _1, _1>>{},
-                                Tile<_16,_16,_8>{});
+                                Tile<_16,_32,_8>{});
     ThrMMA thr_mma = mma.get_slice(0);
 
     Tensor tc = thr_mma.partition_C(c);
