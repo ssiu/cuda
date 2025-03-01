962a75e8f3d529796c70848ce1f325dd3606e0cf
steve <stevesiu1013@gmail.com>
2025-03-01 13:25:27 +1100

    fix

--- cutlass/tensor_op.cu
+++ cutlass/tensor_op.cu
@@ -5,14 +5,14 @@ using namespace cute;
 int main()
 {
     int A[24] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-    Tensor a = make_tensor(&A[0], make_shape(Int<4>{}, Int<3>{}, Int<2>{}), make_stride(Int<6>{}, Int<2>{}, Int<1>{}));
+    Tensor a = make_tensor(&A[0], make_shape(Int<4>{}, Int<6>{}), make_stride(Int<6>{}, Int<1>{}));
     print_tensor(a); // (2, 2, 2)
     print_tensor(a(_,0,_)); // (2, 2)
     printf("A[2] is %d,  size of A is %d\n", *(a.data()+2), a.size());
 
-    Tensor b = make_tensor(&A[0], make_shape(Int<2>{}, Int<3>{}, Int<4>{}), make_stride(Int<1>{}, Int<2>{}, Int<6>{}));
-    print_tensor(b); // (2, 2, 2)
-    print_tensor(b(_,0,_)); // (2, 2)
+//     Tensor b = make_tensor(&A[0], make_shape(Int<2>{}, Int<3>{}, Int<4>{}), make_stride(Int<1>{}, Int<2>{}, Int<6>{}));
+//     print_tensor(b); // (2, 2, 2)
+//     print_tensor(b(_,0,_)); // (2, 2)
 
 
 //     Tensor b = local_tile(a(_, 0, _), Shape<Int<1>, Int<2>>{},
@@ -22,7 +22,10 @@ int main()
 //     b(0,0,1) = 10;
 //     print_tensor(b);
 
+    // A is a 4 x 6 tensor
+    Tensor gA = local_tile(A, Shape<Int<2>, Int<2>>{},
+                           make_coord(_, _));  // (128, 16, seq_len / 16)
 
-    
+    print_tensor(gA);
 
 }
