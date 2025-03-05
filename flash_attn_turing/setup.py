aa092b84019407e406a17721a8805c36f16857fd
steve <stevesiu1013@gmail.com>
2025-03-05 18:07:43 +0800

    fix

--- flash_attn_turing/setup.py
+++ flash_attn_turing/setup.py
@@ -44,7 +44,8 @@ setup(
                      #"flash_fwd_v11.cu"
                      #"flash_fwd_v12.cu"
                      #"flash_fwd_v13.cu"
-                     "flash_fwd_v14.cu"
+                     #"flash_fwd_v14.cu"
+                     "flash_fwd_v15.cu"
                      ],
             include_dirs=cutlass_include_dirs,
             extra_compile_args={'nvcc': nvcc_flags}
