1b232b56a58d98d47b5d8b4928b9d57da4f080cc
steve <stevesiu1013@gmail.com>
2025-03-07 23:42:26 +1100

    fix

--- flash_attn_turing/benchmark.sh
+++ flash_attn_turing/benchmark.sh
@@ -27,8 +27,16 @@ pip install .
 #    ncu --metrics sm__throughput.avg.pct_of_peak_sustained_elapsed \
 #        --csv ./launch_sm75_gemm.o "$size" "$size" "$size" > "${size}.csv"
 
+sizes=(512 1024 2048 4096 8192 16384)
 
-ncu --metrics gpu__time_duration.sum,sm__throughput.avg.pct_of_peak_sustained_elapsed --csv python benchmark_flash.py 4 4096 32 128 > "profile.csv"
+for size in "${sizes[@]}"; do
+    echo "Running with size $size..."
+
+    ncu --metrics gpu__time_duration.sum,sm__throughput.avg.pct_of_peak_sustained_elapsed \
+        --csv python benchmark_flash.py 4 "$size" 32 128 > "${size}.csv"
+
+    echo "Output saved to ${size}.csv"
+
 #ncu --metrics gpu__time_duration.sum,sm__throughput.avg.pct_of_peak_sustained_elapsed python benchmark_flash.py
 
 # google cloud
