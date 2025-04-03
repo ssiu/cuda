d1e62922e672c0ca1821d8f45fdff3b9c59716e0
steve <stevesiu1013@gmail.com>
2025-04-03 15:41:33 +1100

    fix

--- cutlass/tensor_mma.cu
+++ cutlass/tensor_mma.cu
@@ -41,20 +41,20 @@ int main()
     for (int i=0; i< tc(make_coord(_,0),_,_).size(); i++) {
         printf("%d\n", tc(make_coord(_,0),_,_)[i]);
     }
-
+    print("=====\n");
     for (int i=0; i< tc(make_coord(_,1),_,_).size(); i++) {
         printf("%d\n", tc(make_coord(_,1),_,_)[i]);
     }
-
+    print("=====\n");
     for (int i=0; i< tc(make_coord(0,_),_,_).size(); i++) {
         printf("%d\n", tc(make_coord(0,_),_,_)[i]);
     }
-
+    print("=====\n");
     for (int i=0; i< tc(make_coord(1,_),_,_).size(); i++) {
         printf("%d\n", tc(make_coord(1,_),_,_)[i]);
     }
+    print("=====\n");
 
-    printf("hello\n");
     Tensor td = tc;
     print(td);
 
