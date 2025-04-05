63e72c6ac151e32cd1fc5147eb20381c2fd93fe6
steve <stevesiu1013@gmail.com>
2025-04-06 00:03:29 +1100

    fix

--- cpp_extension_simple/simple.cpp
+++ cpp_extension_simple/simple.cpp
@@ -9,13 +9,13 @@ std::vector<torch::Tensor> Dummy(int const N) {
     return outputs;
 }
 
-std::vector<torch::Tensor> Dummy(int const N) {
-    std::vector<torch::Tensor> outputs;
-    auto out = torch::zeros({1,2}, torch::dtype(torch::kFloat32));
-    for (int n=0; n<N; n++)
-        outputs.push_back(out.clone());
-    return outputs;
-}
+//std::vector<torch::Tensor> Dummy(int const N) {
+//    std::vector<torch::Tensor> outputs;
+//    auto out = torch::zeros({1,2}, torch::dtype(torch::kFloat32));
+//    for (int n=0; n<N; n++)
+//        outputs.push_back(out.clone());
+//    return outputs;
+//}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("Dummy", &Dummy, "Dummy function.");
