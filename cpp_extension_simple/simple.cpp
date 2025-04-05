e05f629eae32d6c255789bc7cabe7fae81f710a0
steve <stevesiu1013@gmail.com>
2025-04-06 00:02:25 +1100

    fix

--- cpp_extension_simple/simple.cpp
+++ cpp_extension_simple/simple.cpp
@@ -1,5 +1,13 @@
 #include <torch/extension.h>
-using namespace std;
+//using namespace std;
+
+std::vector<torch::Tensor> Dummy(int const N) {
+    std::vector<torch::Tensor> outputs;
+    auto out = torch::zeros({1,2}, torch::dtype(torch::kFloat32));
+    for (int n=0; n<N; n++)
+        outputs.push_back(out.clone());
+    return outputs;
+}
 
 std::vector<torch::Tensor> Dummy(int const N) {
     std::vector<torch::Tensor> outputs;
