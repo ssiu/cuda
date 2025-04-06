75d7184b2a53121b3989790b6b62963d157f56dc
steve <stevesiu1013@gmail.com>
2025-04-06 14:45:53 +1000

    fix

--- cpp_extension_simple/simple.cpp
+++ cpp_extension_simple/simple.cpp
@@ -9,12 +9,12 @@
 //    return outputs;
 //}
 
-std::vector<torch::Tensor> Dummy(int const N) {
+std::vector<torch::Tensor> Dummy(torch::Tensor t, int const N) {
     std::vector<torch::Tensor> outputs;
     auto out = torch::empty({1,2}, torch::dtype(torch::kFloat32));
     for (int n=0; n<N; n++)
         outputs.push_back(out.clone());
-    return {outputs[0], outputs[1]};
+    return {t, t};
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- cpp_extension_simple/test.py
+++ cpp_extension_simple/test.py
@@ -2,6 +2,7 @@ import torch
 import simple
 # outputs = simple.Dummy(3)
 # print(outputs)
+t = torch.randn(2,3, dtype=torch.float16).to("cuda")
 a, b = simple.Dummy(2)
 print(a)
 print(b)
