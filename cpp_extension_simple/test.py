184e5c1f9dfe1e35400e2a46f4f6bc4202b4cdf5
steve <stevesiu1013@gmail.com>
2025-04-06 14:47:11 +1000

    fix

--- cpp_extension_simple/test.py
+++ cpp_extension_simple/test.py
@@ -3,6 +3,6 @@ import simple
 # outputs = simple.Dummy(3)
 # print(outputs)
 t = torch.randn(2,3, dtype=torch.float16).to("cuda")
-a, b = simple.Dummy(2)
+a, b = simple.Dummy(t, 2)
 print(a)
 print(b)
